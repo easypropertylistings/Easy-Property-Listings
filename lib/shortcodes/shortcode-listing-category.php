3e5804fdcf87d294f0c116dd8be35ead9785123e
mediatelecom <mediatelecomuk@gmail.com>
2014-07-04 17:44:32 +0530

    Create shortcode-listing-category.php

---
+++ lib/shortcodes/shortcode-listing-category.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * SHORTCODE :: Listing Category [listing_category]
+ *
+ * @package     EPL
+ * @subpackage  Shotrcode/map
+ * @copyright   Copyright (c) 2014, Merv Barrett
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Only load on front
+if( is_admin() ) {
+	return;
+}
+/**
+ * This shortcode allows for you to specify the property type(s) using 
+ * [listing_category post_type="property" status="current,sold,leased" category_key="property_rural_category" category="farm"] option. You can also 
+ * limit the number of entries that display. using  [listing_category limit="5"]
+ */
+function epl_shortcode_listing_category_callback( $atts ) {
+	extract( shortcode_atts( array(
+		'post_type' 	=>	'',
+		'status'		=>	array('current' , 'sold' , 'leased' ),
+		'category_key'	=>	'',
+		'category'		=>	'',
+		'limit'			=>	'10', // Number of maximum posts to show
+	), $atts ) );
+	
+	if(empty($post_type)) {
+		return;
+	}
+	
+	ob_start();
+	$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+	$args = array(
+		'post_type' 		=>	$post_type,
+		'posts_per_page'	=>	$limit,
+		'paged' 			=>	$paged
+	);
+	
+	if(!empty($status)) {
+		if(!is_array($status)) {
+			$status = explode(",", $status);
+			$status = array_map('trim', $status);
+			
+			$args['meta_query'][] = array(
+				'key' => 'property_status',
+				'value' => $status,
+				'compare' => 'IN'
+			);
+		}
+	}
+	
+	if(!empty($category_key) && !empty($category)) {
+		if(!is_array($category)) {
+			$category = explode(",", $category);
+			$category = array_map('trim', $category);
+			
+			$args['meta_query'][] = array(
+				'key' => $category_key,
+				'value' => $category,
+				'compare' => 'IN'
+			);
+		}
+	}
+	
+	$query_open = new WP_Query( $args );
+	if ( $query_open->have_posts() ) { ?>
+		<div class="loop">
+			<div class="loop-content">
+				<?php
+					while ( $query_open->have_posts() ) {
+						$query_open->the_post();
+						echo epl_property_blog_slim();
+					}
+				?>
+			</div>
+			<div class="loop-footer">
+				<!-- Previous/Next page navigation -->
+				<div class="loop-utility clearfix">
+					<div class="alignleft"><?php previous_posts_link( __( '&laquo; Previous Page', 'epl' ), $query_open->max_num_pages ); ?></div>
+					<div class="alignright"><?php next_posts_link( __( 'Next Page &raquo;', 'epl' ), $query_open->max_num_pages ); ?></div>
+				</div>
+			</div>
+		</div>
+		<?php
+	} else {
+		echo '<h3>'.__('Nothing found, please check back later.', 'epl').'</h3>';
+	}
+	wp_reset_postdata();
+	return ob_get_clean();
+}
+add_shortcode( 'listing_category', 'epl_shortcode_listing_category_callback' );
