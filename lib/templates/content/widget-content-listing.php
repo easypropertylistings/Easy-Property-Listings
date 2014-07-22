fc9bc36b97baf4623f4df2599516bdfa8256048b
Merv <merv@realestateconnected.com.au>
2014-07-22 10:41:09 +0800

    Added Options to Listing Widget
    New: Added options to show hide:
    Suburb
    Street
    Excerpt
    Price
    Read More
    Customise Read More Label

--- lib/templates/content/widget-content-listing.php
+++ lib/templates/content/widget-content-listing.php
@@ -10,7 +10,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 
-<div id="post-<?php the_ID(); ?>" class="property-widget-image" <?php //post_class('property-widget-image'); ?>>
+<div id="post-<?php the_ID(); ?>" class="epl-listing-widget property-widget-image">
 	<div class="entry-header">
 		<?php if ( has_post_thumbnail() ) : ?>
 			<div class="epl-img-widget">
@@ -24,8 +24,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	<div class="entry-content">
 		<?php // Heading Options
 		if ($title == 'on') { ?>
-			<h5 class="property-heading"><?php echo $the_property_heading; ?></h5>
+			<h5 class="property-heading"><a href="<?php the_permalink; ?>"><?php echo $the_property_heading; ?></a></h5>
 		<?php } ?>
+		<?php if ( $d_excerpt == 'on' ) { 
+			the_excerpt(); 
+		} ?>
 		
 		<!-- Address -->
 		<div class="property-address">
@@ -44,32 +47,45 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 							<span class="item-state"><?php echo $property_address_state . ' '; ?></span>
 							<span class="item-pcode"><?php echo $property_address_postal_code; ?></span>
 						</div>
-					<?php } else { ?>
-						<div class="property-meta suburb-name"><?php echo $property_address_suburb; ?></div>
-						<div class="property-meta street-name"><?php echo $property_address_street; ?></div>
 					<?php }
 				} else {
 					// Address Display not Commercial or Business type
 					if ( $property_address_display == 'yes' ) { ?>
-						<div class="property-meta suburb-name"><?php echo $property_address_suburb; ?></div>
-						<div class="property-meta street-name"><?php echo $property_address_street; ?></div>
+						<?php // Suburb
+						if ( $d_suburb == 'on' ) { ?>
+							<div class="property-meta suburb-name"><?php echo $property_address_suburb; ?></div>
+						<?php } ?>
+						
+						<?php // Street
+						if ( $d_street == 'on' ) { ?>
+							<div class="property-meta street-name"><?php echo $property_address_street; ?></div>
+						<?php } ?>
 					<?php } else { ?>
-						<div class="property-meta suburb-name"><?php echo $property_address_suburb; ?></div>
+						<?php // Suburb
+						if ( $d_suburb == 'on' ) { ?>
+							<div class="property-meta suburb-name"><?php echo $property_address_suburb; ?></div>
+						<?php } ?>
 					<?php } 
 				} ?>
 		</div>
-		<!-- END Address -->
 
 		<?php // Icon Options
-		if ($icons == 'all') { ?>
+		if ( $icons == 'all' ) { ?>
 			<div class="property-meta property-feature-icons"><?php echo $property_icons_full; ?></div>
 		<?php } elseif ($icons == 'bb') { ?>
 			<div class="property-meta property-feature-icons"><?php echo $property_icons_bb; ?></div>
-		<?php } ?>		
-
-		<div class="property-meta price"><?php echo $price; ?></div>
-		<form class="epl-property-button" action="<?php the_permalink(); ?>" method="post">
-			<input type=submit value="<?php _e('Read More', 'epl'); ?>" />
-		</form>
+		<?php } ?>
+		
+		<?php // Price
+		if ( $d_price == 'on') { ?>
+			<div class="property-meta price"><?php echo $price; ?></div>
+		<?php } ?>
+		
+		<?php // Read More
+		if ( $d_more == 'on') { ?>
+			<form class="epl-property-button" action="<?php the_permalink(); ?>" method="post">
+				<input type=submit value="<?php echo $more_text; ?>" />
+			</form>
+		<?php } ?>
 	</div>
 </div>
