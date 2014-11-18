d479a75098e28d604a46d8d13163f425661447bb
mediatelecom <mediatelecomuk@gmail.com>
2014-11-18 15:20:54 +0530

    dashboard widget css fixes

--- lib/widgets/widget-dashboard-epl-status.php
+++ lib/widgets/widget-dashboard-epl-status.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * Add Epl Status widget to the dashboard.
+ * DASHBOARD WIDGET :: Easy Property Listings Status
  *
- * This function is hooked into the 'wp_dashboard_setup' action below.
+ * @package     EPL
+ * @subpackage  dashbaord Widget/status
+ * @copyright   Copyright (c) 2014, Merv Barrett
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.3
  */
+
+// Exit if accessed directly
 function example_add_dashboard_widgets() {
 
 	wp_add_dashboard_widget(
@@ -26,12 +32,9 @@ function epl_status_dashboard_widget_callback() {
 					<li class="epl_type_<?php echo $activate_post_type; ?>">
 						<a href="edit.php?post_type=<?php echo $activate_post_type; ?>">
 							<strong><?php echo epl_get_plural($count->publish,$activate_post_type); ?></strong>
+							<?php epl_posts_highlights($activate_post_type);?>
 						</a>
-						<div class="epl_posts_highlights">
-							<?php
-								epl_posts_highlights($activate_post_type);
-							?>
-						</div>
+						
 					</li><?php
 				}
 			}
@@ -43,22 +46,22 @@ function epl_status_dashboard_widget_callback() {
 function epl_get_plural($count,$singular) {
 	switch($singular){
 		case 'property':
-			return sprintf( _n( '1 Property', '%s Properties', $count, 'epl' ), $count );
+			return sprintf( _n( '1 Property', '%s Property', $count, 'epl' ), $count );
 		break;
 		case 'land':
-			return sprintf( _n( '1 Land', '%s Lands', $count, 'epl' ), $count );
+			return sprintf( _n( '1 Land', '%s Land', $count, 'epl' ), $count );
 		break;
 		case 'rural':
-			return sprintf( _n( '1 Rural', '%s Rurals', $count, 'epl' ), $count );
+			return sprintf( _n( '1 Rural', '%s Rural', $count, 'epl' ), $count );
 		break;
 		case 'commercial':
-			return sprintf( _n( '1 Commercial', '%s Commercials', $count, 'epl' ), $count );
+			return sprintf( _n( '1 Commercial', '%s Commercial', $count, 'epl' ), $count );
 		break;
 		case 'rental':
-			return sprintf( _n( '1 Rental', '%s Rentals', $count, 'epl' ), $count );
+			return sprintf( _n( '1 Rental', '%s Rental', $count, 'epl' ), $count );
 		break;
 		default:
-			return sprintf( _n( '1 '.$singular, '%s '.$singular.'s', $count, 'epl' ), $count );
+			return sprintf( _n( '1 '.$singular, '%s '.$singular, $count, 'epl' ), $count );
 		break;
 	}
 	
@@ -81,64 +84,7 @@ function epl_get_post_count($type,$meta_key,$meta_value) {
 
 function epl_posts_highlights($type) {
 	switch($type){
-		case 'property':
-			$filters = array(
-							'property_status'		=>	array('value'	=>	'sold','string'	=>	__('Sold','epl')),
-							'property_authority'	=>	array('value'	=>	'sale','string'	=>	__('On Sale','epl')),
-							'property_under_offer'	=>	array('value'	=>	'yes','string'	=>	__('Under Offer','epl')),
-						);
-			foreach($filters as $filter_key 	=>	$filter_value){
-				$count = epl_get_post_count($type,$filter_key,$filter_value['value']);
-				if($count != 0){
-					echo '<span>'.$count.' '.$filter_value['string'].' </span>';
-				}
-				
-			}
-			
-		break;
-		case 'land':
-			$filters = array(
-							'property_status'		=>	array('value'	=>	'sold','string'	=>	__('Sold','epl')),
-							'property_authority'	=>	array('value'	=>	'sale','string'	=>	__('On Sale','epl')),
-							'property_under_offer'	=>	array('value'	=>	'yes','string'	=>	__('Under Offer','epl')),
-						);
-			foreach($filters as $filter_key 	=>	$filter_value){
-				$count = epl_get_post_count($type,$filter_key,$filter_value['value']);
-				if($count != 0){
-					echo '<span>'.$count.' '.$filter_value['string'].' </span>';
-				}
-				
-			}
-		break;
-		case 'rural':
-			$filters = array(
-							'property_status'		=>	array('value'	=>	'sold','string'	=>	__('Sold','epl')),
-							'property_authority'	=>	array('value'	=>	'sale','string'	=>	__('On Sale','epl')),
-							'property_under_offer'	=>	array('value'	=>	'yes','string'	=>	__('Under Offer','epl')),
-						);
-			foreach($filters as $filter_key 	=>	$filter_value){
-				$count = epl_get_post_count($type,$filter_key,$filter_value['value']);
-				if($count != 0){
-					echo '<span>'.$count.' '.$filter_value['string'].' </span>';
-				}
-				
-			}
-		break;
 		case 'commercial':
-			$filters = array(
-							'property_status'		=>	array('value'	=>	'sold','string'		=>	__('Sold','epl')),
-							'property_status'		=>	array('value'	=>	'leased','string'	=>	__('Leased','epl')),
-							'property_authority'	=>	array('value'	=>	'sale','string'		=>	__('On Sale','epl')),
-							'property_under_offer'	=>	array('value'	=>	'yes','string'		=>	__('Under Offer','epl')),
-						);
-			foreach($filters as $filter_key 	=>	$filter_value){
-				$count = epl_get_post_count($type,$filter_key,$filter_value['value']);
-				if($count != 0){
-					echo '<span>'.$count.' '.$filter_value['string'].' </span>';
-				}
-				
-			}
-		break;
 		case 'rental':
 			$filters = array(
 							'property_status'		=>	array('value'	=>	'sold','string'	=>	__('Sold','epl')),
@@ -154,6 +100,9 @@ function epl_posts_highlights($type) {
 				
 			}
 		break;
+		case 'property':
+		case 'land':
+		case 'rural':
 		default:
 			$filters = array(
 							'property_status'		=>	array('value'	=>	'sold','string'	=>	__('Sold','epl')),
