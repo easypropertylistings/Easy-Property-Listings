55b8cb2d537a4c07599e0b8bf64f87e3db4ba366
Merv <support@easypropertylistings.com.au>
2014-12-04 08:35:46 +0800

    Fixed content/ into EPL_PATH_TEMPLATES_CONTENT

--- lib/includes/template-functions.php
+++ lib/includes/template-functions.php
@@ -315,7 +315,7 @@ function epl_property_author_box_simple_grav() {
 		$author_style = $epl_settings['epl_staff_link_to'];
 	}
 	
-	include( 'content/content-author-box-simple-grav.php' );
+	include( EPL_PATH_TEMPLATES_CONTENT.'content-author-box-simple-grav.php' );
 }
 
 // AUTHOR LISTING CARDS : Listing Card
