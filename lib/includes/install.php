0ad1ab2860058a8e1fb1f9d03cec4f0137bb2f51
mediatelecom <mediatelecomuk@gmail.com>
2015-01-12 11:18:46 +0530

    minor fixes

--- lib/includes/install.php
+++ lib/includes/install.php
@@ -163,6 +163,7 @@ function epl_plugin_updates() {
 	$current_version = get_option( 'epl_version' );
 	if ( version_compare( $current_version, '1.3', '<' ) ) {
 		include( EPL_PATH_UPDATES.'epl-1.3.1.php' );
+		update_option( 'epl_version' ,$current_version);
 	}
 }
 add_action( 'admin_init', 'epl_plugin_updates' );
