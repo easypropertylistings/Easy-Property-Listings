bdaa4728703029c0fa7f203b9e09d654e0824d4d
Merv <merv@realestateconnected.com.au>
2015-06-03 12:55:04 +0800

    Translation prefix

--- lib/includes/functions.php
+++ lib/includes/functions.php
@@ -1191,7 +1191,7 @@ function epl_admin_sidebar () {
  */
 function epl_sold_label_status_filter_callback() {
 	global $epl_settings;
-	$sold_label	= $epl_settings['label_sold'] != 'Sold' || $epl_settings['label_sold'] != '' ? $epl_settings['label_sold'] : 'Sold';
+	$sold_label	= $epl_settings['label_sold'] != 'Sold' || $epl_settings['label_sold'] != '' ? $epl_settings['label_sold'] : __('Sold' , 'epl');
 	return $sold_label;
 }
 add_filter('epl_sold_label_status_filter', 'epl_sold_label_status_filter_callback' );
@@ -1203,7 +1203,7 @@ add_filter('epl_sold_label_status_filter', 'epl_sold_label_status_filter_callbac
  */
 function epl_under_offer_label_status_filter_callback() {
 	global $epl_settings;
-	$under_offer_label	= $epl_settings['label_under_offer'] != 'Under Offer' || $epl_settings['label_under_offer'] != '' ? $epl_settings['label_under_offer'] : 'Under Offer';
+	$under_offer_label	= $epl_settings['label_under_offer'] != 'Under Offer' || $epl_settings['label_under_offer'] != '' ? $epl_settings['label_under_offer'] : __('Under Offer' , 'epl');
 	return $under_offer_label;
 }
 add_filter('epl_under_offer_label_status_filter', 'epl_under_offer_label_status_filter_callback' );
@@ -1215,7 +1215,7 @@ add_filter('epl_under_offer_label_status_filter', 'epl_under_offer_label_status_
  */
 function epl_leased_label_status_filter_callback() {
 	global $epl_settings;
-	$leased_label	= $epl_settings['label_leased'] != 'Leased' || $epl_settings['label_leased'] != '' ? $epl_settings['label_leased'] : 'Leased';
+	$leased_label	= $epl_settings['label_leased'] != 'Leased' || $epl_settings['label_leased'] != '' ? $epl_settings['label_leased'] : __('Leased' , 'epl');
 	return $leased_label;
 }
 add_filter('epl_leased_label_status_filter', 'epl_leased_label_status_filter_callback' );
