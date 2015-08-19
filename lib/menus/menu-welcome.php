640728c4402c24f9a4d678deaae020288f967c73
Merv <merv@realestateconnected.com.au>
2015-08-20 00:18:18 +0800

    WordPress 4.3 widget construct

--- lib/menus/menu-welcome.php
+++ lib/menus/menu-welcome.php
@@ -270,6 +270,11 @@ class EPL_Welcome {
 			
 				<div class="feature-section">
 				
+					<h4><?php _e( 'Version 2.2.5', 'epl' );?></h4>
+					<ul>
+						<li><?php _e( 'Fix: Widget construct fixes for WordPress 4.3.', 'epl' );?></li>
+					</ul>
+				
 					<h4><?php _e( 'Version 2.2.4', 'epl' );?></h4>
 					<ul>
 						<li><?php _e( 'Tweak: Improvements to Commercial/Commercial Land/Business pricing when set to Lease type to display free form price text.', 'epl' );?></li>
