23b8acb1ade4c00da2d9bbc94cf639ebc14bb760
taher <tah3r.a@gmail.com>
2015-11-09 16:59:46 +0330

    Adding author to WP_Query in listing shortcode to filtering specific author listigns.

--- lib/shortcodes/shortcode-listing.php
+++ lib/shortcodes/shortcode-listing.php
@@ -14,11 +14,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Only load on front
 if( is_admin() ) {
-	return; 
+	return;
 }
 /**
- * This shortcode allows for you to specify the property type(s) using 
- * [listing post_type="property,rental" status="current,sold,leased" template="default"] option. You can also 
+ * This shortcode allows for you to specify the property type(s) using
+ * [listing post_type="property,rental" status="current,sold,leased" template="default"] option. You can also
  * limit the number of entries that display. using  [listing limit="5"]
  */
 function epl_shortcode_listing_callback( $atts ) {
@@ -26,46 +26,53 @@ function epl_shortcode_listing_callback( $atts ) {
 	if(!empty($property_types)) {
 		 $property_types = array_keys($property_types);
 	}
-	
+
 	extract( shortcode_atts( array(
-		'post_type' 		=>	$property_types, //Post Type
-		'status'		=>	array('current' , 'sold' , 'leased' ),
-		'limit'			=>	'10', // Number of maximum posts to show
-		'template'		=>	false, // Template can be set to "slim" for home open style template
-		'location'		=>	'', // Location slug. Should be a name like sorrento
-		'tools_top'		=>	'off', // Tools before the loop like Sorter and Grid on or off
-		'tools_bottom'		=>	'off', // Tools after the loop like pagination on or off
-		'sortby'		=>	'', // Options: price, date : Default date
-		'sort_order'		=>	'DESC',
-		'query_object'		=>	'' // only for internal use . if provided use it instead of custom query 
+		'post_type'    => $property_types, //Post Type
+		'status'       => array('current' , 'sold' , 'leased' ),
+		'limit'        => '10', // Number of maximum posts to show
+		'author'       => '',	// Author of listings.
+		'template'     => false, // Template can be set to "slim" for home open style template
+		'location'     => '', // Location slug. Should be a name like sorrento
+		'tools_top'    => 'off', // Tools before the loop like Sorter and Grid on or off
+		'tools_bottom' => 'off', // Tools after the loop like pagination on or off
+		'sortby'       => '', // Options: price, date : Default date
+		'sort_order'   => 'DESC',
+		'query_object' => '', // only for internal use . if provided use it instead of custom query
 	), $atts ) );
-	
+
 	if(is_string($post_type) && $post_type == 'rental') {
 		$meta_key_price = 'property_rent';
 	} else {
 		$meta_key_price = 'property_price';
 	}
-	
+
 	$sort_options = array(
 		'price'			=>	$meta_key_price,
-		'date'			=>	'post_date'
+		'date'			=>	'post_date',
 	);
-	if( !is_array($post_type) ) {
-		$post_type 			= array_map('trim',explode(',',$post_type) );
+	if ( ! is_array( $post_type ) ) {
+		$post_type = array_map( 'trim',explode( ',',$post_type ) );
 	}
 	ob_start();
-	$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+	$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 	$args = array(
-		'post_type' 		=>	$post_type,
-		'posts_per_page'	=>	$limit,
-		'paged' 		=>	$paged
+		'post_type'      =>	$post_type,
+		'posts_per_page' =>	$limit,
+		'paged'          =>	$paged
 	);
-	
+	if ( ! empty( $author ) ) {
+		if ( is_array( $author ) ) {
+			$author = implode( ',', array_map( 'absint', $author ) );
+		}
+		$args['author'] = trim( $author );
+	}
+
 	if(!empty($location) ) {
 		if( !is_array( $location ) ) {
 			$location = explode(",", $location);
 			$location = array_map('trim', $location);
-			
+
 			$args['tax_query'][] = array(
 				'taxonomy'	=> 'location',
 				'field'		=> 'slug',
@@ -73,24 +80,24 @@ function epl_shortcode_listing_callback( $atts ) {
 			);
 		}
 	}
-	
+
 	if(!empty($status)) {
 		if(!is_array($status)) {
 			$status = explode(",", $status);
 			$status = array_map('trim', $status);
-			
+
 			$args['meta_query'][] = array(
 				'key'		=> 'property_status',
 				'value'		=> $status,
 				'compare'	=> 'IN'
 			);
-			
+
 			add_filter('epl_sorting_options','epl_sorting_options_callback');
 		}
 	}
 
 	if( $sortby != '' ) {
-	
+
 		if($sortby == 'price') {
 			$args['orderby']	=	'meta_value_num';
 			$args['meta_key']	=	$meta_key_price;
@@ -101,7 +108,7 @@ function epl_shortcode_listing_callback( $atts ) {
 		}
 		$args['order']			=	$sort_order;
 	}
-	
+
 	if( isset( $_GET['sortby'] ) ) {
 		$orderby = sanitize_text_field( trim($_GET['sortby']) );
 		if($orderby == 'high') {
@@ -127,15 +134,15 @@ function epl_shortcode_listing_callback( $atts ) {
 			$args['meta_key']	=	'property_status';
 			$args['order']		=	'ASC';
 		}
-		
+
 	}
 
 	$query_open = new WP_Query( $args );
-	
+
 	if( is_object($query_object) ) {
 		$query_open = $query_object;
 	}
-	
+
 	if ( $query_open->have_posts() ) { ?>
 		<div class="loop epl-shortcode">
 			<div class="loop-content epl-shortcode-listing <?php echo epl_template_class( $template ); ?>">
@@ -170,7 +177,7 @@ function epl_sorting_options_callback($sorters) {
 	foreach($sorters as $key	=>	 &$sorter) {
 		if($sorter['id'] == 'status_asc' || $sorter['id'] == 'status_desc') {
 			unset($sorters[$key]);
-		}	
+		}
 	}
 	return $sorters;
 }
