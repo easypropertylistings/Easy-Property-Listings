9c3f4b440e79bcec291504c69765251c458cbfef
epl-dev <developer@realestateconnected.com.au>
2016-04-07 09:57:54 +0530

    property title search fixed

--- lib/widgets/widget-functions.php
+++ lib/widgets/widget-functions.php
@@ -808,10 +808,13 @@ function epl_search( WP_Query &$query, array $data = array(), $get_posts = false
 	$query->set( 'posts_per_page', get_option( 'posts_per_page' ) );
 	$query->set( 'paged', $paged );
 
-	if ( isset( $data['property_id'] ) ) {
-		if ( absint( $data['property_id'] ) ) {
-			$query->set( 'epl_post_title', absint( $data['property_id'] ) );
+	if(isset($data['property_id']) ) {
+		if(is_numeric($data['property_id'])) {
+			
+		} else {
+			$query->set( 'epl_post_title', sanitize_text_field($data['property_id']) );
 		}
+			
 	}
 
 	if ( isset( $data['property_agent'] ) ) {
