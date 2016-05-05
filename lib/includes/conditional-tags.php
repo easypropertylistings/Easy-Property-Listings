13d6751f316f5dd3fd6381c1671b2d40417fa4bd
epl-dev <developer@realestateconnected.com.au>
2016-05-05 12:01:05 +0530

    fix for conditional tags when none epl posts are active

--- lib/includes/conditional-tags.php
+++ lib/includes/conditional-tags.php
@@ -87,6 +87,11 @@ function is_epl_rental_post($type=null) {
  * @since 2.2
  */
 function is_epl_post($type=null) {
+
+	if ( empty(epl_all_post_types()) )
+	return false;
+
+
 	$type = $type === null ? get_post_type() : $type;
 	return 	in_array($type,epl_all_post_types());
 }
@@ -97,6 +102,10 @@ function is_epl_post($type=null) {
  * @since 2.2
  */
 function is_epl_post_single() {
+
+	if ( empty(epl_all_post_types()) )
+		return false;
+
 	return 	is_singular(epl_all_post_types());
 }
 
@@ -106,6 +115,10 @@ function is_epl_post_single() {
  * @since 2.2
  */
 function is_epl_post_type($type) {
+
+	if ( empty(epl_all_post_types()) )
+	return false;
+
 	return 	(in_array($type,epl_all_post_types()) && get_post_type() == $type );
 }
 
@@ -115,5 +128,9 @@ function is_epl_post_type($type) {
  * @since 2.2
  */
 function is_epl_post_archive() {
+
+	if ( empty(epl_all_post_types()) )
+	return false;
+
 	return 	is_post_type_archive(epl_all_post_types());
 }
