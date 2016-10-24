8595d9ac82246c0b4bd884e3f86adb7b20ee944e
Merv Barrett <merv@realestateconnected.com.au>
2016-10-24 11:46:23 +0800

    Update easy-property-listings.php

--- easy-property-listings.php
+++ easy-property-listings.php
@@ -91,7 +91,7 @@ if ( ! class_exists( 'Easy_Property_Listings' ) ) :
 		public function setup_constants() {
 			// Plugin version
 			if ( ! defined( 'EPL_PROPERTY_VER' ) ) {
-				define( 'EPL_PROPERTY_VER', '3.0.97' );
+				define( 'EPL_PROPERTY_VER', '3.0.98' );
 			}
 			// Plugin DB version
 			if ( ! defined( 'EPL_PROPERTY_DB_VER' ) ) {
