b4814c28155152628e96dea8da2c473a916ebd3e
Merv Barrett <mervb1@gmail.com>
2016-11-10 13:55:58 +0800

    Internal Listing Search Commercial notations

--- lib/shortcodes/shortcode-listing-search-commercial.php
+++ lib/shortcodes/shortcode-listing-search-commercial.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * SHORTCODE :: Listing Search [listing_search]
+ * SHORTCODE :: Listing Search [listing_search_commercial]
  *
  * @package     EPL
  * @subpackage  Shortcode/Listing Search
  * @copyright   Copyright (c) 2014, Merv Barrett
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       1.2
+ * @since       3.1
  */
 
 // Exit if accessed directly
@@ -14,9 +14,9 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
  * This shortcode allows for you to specify the property type(s) using
- * [listing_search title="" post_type="property" property_status="current/sold/leased" search_house_category="on/off" search_price="on/off" search_bed="on/off" search_bath="on/off" search_car="on/off" search_other="on/off"] option
+ * [listing_search_commercial title="" post_type="property" property_status="current/sold/leased" search_house_category="on/off" search_price="on/off" search_bed="on/off" search_bath="on/off" search_car="on/off" search_other="on/off"] option
  *
- * @since       1.2
+ * @since       3.1
  */
 function epl_shortcode_listing_search_commercial_callback( $atts ) {
 	$attributes = shortcode_atts( epl_listing_search_commercial_get_defaults(), $atts );
