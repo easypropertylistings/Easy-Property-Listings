0bc343210a956f0d38352c8dc433fb055b31d160
Prince Singh <developer@realestateconnected.com.au>
2016-11-15 15:38:23 +0530

    #348

--- lib/assets/assets.php
+++ lib/assets/assets.php
@@ -71,7 +71,7 @@ function epl_wp_enqueue_scripts() {
 
 	}
 
-	// All CSS includting Structual
+	// All CSS including Structual
 	if( isset($epl_settings['epl_use_core_css']) &&  $epl_settings['epl_use_core_css'] == 'on') {
 
 	} else {
@@ -79,13 +79,21 @@ function epl_wp_enqueue_scripts() {
 		// Structural CSS
 		wp_enqueue_style(	'epl-css-lib', 			$current_dir_path . '/css/style-structure.css',		FALSE,			EPL_PROPERTY_VER );
 
-		// Legacy CSS: on is to disable visual css, default off
-		if( isset($epl_settings['epl_css_legacy']) &&  $epl_settings['epl_css_legacy'] == 'on') {
+		// Visual CSS: on is to disable visual css, default off
+		if( isset($epl_settings['epl_css_visual_disable']) &&  $epl_settings['epl_css_visual_disable'] == 'on') {
 
 			wp_enqueue_style(	'epl-front-styles', 	$current_dir_path . '/css/style-legacy.css',			FALSE,			EPL_PROPERTY_VER );
 
 		} else {
 			wp_enqueue_style(	'epl-style', 		$current_dir_path . '/css/style.css',			FALSE,			EPL_PROPERTY_VER );
+		}
+		
+		/**
+		 * Load theme specific stylesheet for epl, if exists
+		 * Loaded at the end to override any styles in plugin
+		 */
+		if( file_exists(get_stylesheet_directory().'/easypropertylistings/style.css' ) ) {
+			wp_enqueue_style(	'epl-theme-style', 		get_stylesheet_directory_uri().'/easypropertylistings/style.css',			FALSE,			EPL_PROPERTY_VER );
 		}
 	}
 
