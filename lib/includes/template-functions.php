bca126b7b004f4b854ee6ac3fcb4e6e61f04bd9e
Merv Barrett <mervb1@gmail.com>
2018-05-17 09:46:18 +0800

    * New: Added a epl_property_gallery_shortcode filter.

--- lib/includes/template-functions.php
+++ lib/includes/template-functions.php
@@ -1435,8 +1435,9 @@ function epl_property_gallery () {
 			<!-- Gallery -->
 			<div class="epl-gallery-entry entry-gallery epl-clearfix">
 				<?php
-					$d_gallery_n = '[gallery columns="'. $d_gallery_n . '" link="file"]';
-					echo do_shortcode( $d_gallery_n );
+					$gallery_shortcode = '[gallery columns="'. $d_gallery_n . '" link="file"]';
+					$gallery = apply_filters( 'epl_property_gallery_shortcode' , $gallery_shortcode );
+					echo do_shortcode( $gallery );
 				?>
 			</div>
 		</div>
