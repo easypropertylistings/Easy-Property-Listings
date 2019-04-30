0562e361113183adc886ecacdba63db337534dc3
Merv Barrett <mervb1@gmail.com>
2019-04-30 12:58:25 +0800

    Corrected Floorplan filter

--- lib/hooks/hook-floorplan.php
+++ lib/hooks/hook-floorplan.php
@@ -4,7 +4,7 @@
  *
  * @package     EPL
  * @subpackage  Hooks/FloorPlan
- * @copyright   Copyright (c) 2014, Merv Barrett
+ * @copyright   Copyright (c) 2019, Merv Barrett
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
  */
@@ -19,6 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * has floor plans links they will be output on the template
  *
  * @since 1.0
+ * @revised 3.3
  */
 function epl_button_floor_plan() {
 
@@ -37,22 +38,22 @@ function epl_button_floor_plan() {
 
 			<span class="epl-floor-plan-button-wrapper<?php echo $count; ?>">
 
-					<a type="button" 
-						class="fancybox image epl-button epl-floor-plan" 
-						<?php echo apply_filters( 'epl_button_target_floorplan' , '' ); ?> 
+					<a type="button"
+						class="fancybox image epl-button epl-floor-plan"
+						<?php echo apply_filters( 'epl_button_target_floorplan' , '' ); ?>
 						href="<?php echo $link; ?>">
 
-							<?php 
+							<?php
 								$filter_key = str_replace('property_','',$key);
 
 								if( has_filter('epl_button_label_'.$filter_key) ) {
-									$label = apply_filters('epl_button_label_' . $filter_key , $meta_label );
+									$label = apply_filters('epl_button_label_floorplan_' . $filter_key , $meta_label );
 								} else {
-									$label = apply_filters( 'epl_button_label_tour' , $meta_label );
+									$label = apply_filters( 'epl_button_label_floorplan' , $meta_label );
 								}
 								echo $label;
 							?>
-										
+
 					</a>
 			</span> <?php
 
