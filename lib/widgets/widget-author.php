6e3b79300aa5ca45199ae2fb975174a3a4dbaa35
Merv Barrett <mervb1@gmail.com>
2019-04-30 13:25:12 +0800

    Author Box Test

--- lib/widgets/widget-author.php
+++ lib/widgets/widget-author.php
@@ -32,17 +32,19 @@ class EPL_Widget_Author extends WP_Widget {
 			'display'	=>	0,
 			'd_image'	=>	0,
 			'd_icons'	=>	0,
-			'd_bio'		=>	0
+			'd_bio'		=>	0,
+			'author_box'	=>	0,
 		);
 
 		$instance = wp_parse_args( (array) $instance, $defaults );
 
 		extract( $args );
 		$title 		= apply_filters('widget_title', $instance['title']);
-		$display		= $instance['display'];
-		$d_image		= $instance['d_image'];
-		$d_icons		= $instance['d_icons'];
+		$display	= $instance['display'];
+		$d_image	= $instance['d_image'];
+		$d_icons	= $instance['d_icons'];
 		$d_bio		= $instance['d_bio'];
+		$author_box	= $instance['author_box'];
 		$username	= $instance['username'];
 
 		if ( is_epl_post_single() ) {
@@ -50,8 +52,9 @@ class EPL_Widget_Author extends WP_Widget {
 			global $property;
 			$hide_author_box	=	$property->get_property_meta('property_agent_hide_author_box');
 
-			if ( $hide_author_box == 'yes' ) {
+			if ( $hide_author_box == 'yes' && $author_box == 'on' ) {
 				// Hide Author Box
+				// Disabled this feature due to user would not place a widget and hide it.
 			} else {
 				echo $before_widget;
 				if ( $title )
@@ -80,6 +83,7 @@ class EPL_Widget_Author extends WP_Widget {
 		$instance['d_image'] 	= strip_tags($new_instance['d_image']);
 		$instance['d_icons'] 	= strip_tags($new_instance['d_icons']);
 		$instance['d_bio'] 	= strip_tags($new_instance['d_bio']);
+		$instance['author_box'] = strip_tags($new_instance['author_box']);
 		return $instance;
 	}
 
@@ -90,17 +94,19 @@ class EPL_Widget_Author extends WP_Widget {
 			'display'	=>	0,
 			'd_image'	=>	0,
 			'd_icons'	=>	0,
-			'd_bio'		=>	0
+			'd_bio'		=>	0,
+			'author_box'	=>	0
 		);
 
 		$instance = wp_parse_args( (array) $instance, $defaults );
 
 		$title 		= esc_attr($instance['title']);
 		$username 	= esc_attr($instance['username']);
-		$display		= esc_attr($instance['display']);
-		$d_image		= esc_attr($instance['d_image']);
-		$d_icons		= esc_attr($instance['d_icons']);
-		$d_bio		= esc_attr($instance['d_bio']); ?>
+		$display	= esc_attr($instance['display']);
+		$d_image	= esc_attr($instance['d_image']);
+		$d_icons	= esc_attr($instance['d_icons']);
+		$d_bio		= esc_attr($instance['d_bio']);
+		$author_box	= esc_attr($instance['author_box']); ?>
 
 		<p>
 			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'easy-property-listings'); ?></label>
@@ -110,7 +116,7 @@ class EPL_Widget_Author extends WP_Widget {
 		<p>
 			<label for="<?php echo $this->get_field_id('username'); ?>"><?php _e('Username:', 'easy-property-listings'); ?></label>
 			<input autocomplete="off" class="widefat epl-widget-author-username" id="<?php echo $this->get_field_id('username'); ?>" name="<?php echo $this->get_field_name('username'); ?>" type="text" value="<?php echo $username; ?>" placeholder="<?php _e('Type to search users','easy-property-listings'); ?>"/>
-			<span class="help"><?php _e('Comma seperated list of multiple author','easy-property-listings'); ?></span>
+			<span class="help"><?php _e('Search for users, supports multiple','easy-property-listings'); ?></span>
 		</p>
 
 		<p>
@@ -127,6 +133,13 @@ class EPL_Widget_Author extends WP_Widget {
 			<input type="checkbox" id="<?php echo $this->get_field_id('d_bio'); ?>" name="<?php echo $this->get_field_name('d_bio'); ?>" <?php if ($instance['d_bio']) echo 'checked="checked"' ?> />
 			<label for="<?php echo $this->get_field_id('d_bio'); ?>"><?php _e('Display Bio', 'easy-property-listings'); ?></label>
 		</p>
+		<p>
+			<input type="checkbox" id="<?php echo $this->get_field_id('author_box'); ?>" name="<?php echo $this->get_field_name('author_box'); ?>" <?php if ($instance['author_box']) echo 'checked="checked"' ?> />
+			<label for="<?php echo $this->get_field_id('author_box'); ?>"><?php _e('Hide widget if listing has hide author checked', 'easy-property-listings'); ?></label>
+		</p>
+
+
+		<?php echo $instance['author_box']; ?>
 		<?php
 	}
 }
