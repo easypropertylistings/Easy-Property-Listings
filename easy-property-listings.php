6bc075964b85966a997ab98c27b7a5799130bd3f
Merv Barrett <mervb1@gmail.com>
2019-05-23 14:32:34 +0800

    Moved order to other class

--- easy-property-listings.php
+++ easy-property-listings.php
@@ -269,6 +269,7 @@ if ( ! class_exists( 'Easy_Property_Listings' ) ) :
 				require_once EPL_PATH_LIB . 'includes/options-front-end.php';
 				require_once EPL_PATH_LIB . 'shortcodes/shortcode-googlemap.php';
 				require_once EPL_PATH_LIB . 'shortcodes/class-advanced-shortcode-listing.php';
+				require_once EPL_PATH_LIB . 'shortcodes/class-epl-listing-elements.php';
 				require_once EPL_PATH_LIB . 'shortcodes/shortcode-listing.php';
 				require_once EPL_PATH_LIB . 'shortcodes/shortcode-listing-advanced.php';
 				require_once EPL_PATH_LIB . 'shortcodes/shortcode-listing-search.php';
@@ -278,11 +279,6 @@ if ( ! class_exists( 'Easy_Property_Listings' ) ) :
 				require_once EPL_PATH_LIB . 'shortcodes/shortcode-listing-tax-feature.php';
 				require_once EPL_PATH_LIB . 'shortcodes/shortcode-listing-tax-location.php';
 				require_once EPL_PATH_LIB . 'shortcodes/shortcode-listing-auction.php';
-				require_once EPL_PATH_LIB . 'shortcodes/class-epl-listing-elements.php';
-
-				// Partial fix, recommend moving file to shortcodes/ folder and internal docs added to file
-				require_once EPL_PATH_LIB . 'includes/class-epl-listing-elements.php';
-
 				require_once EPL_PATH_LIB . 'hooks/hook-property-map.php';
 				require_once EPL_PATH_LIB . 'hooks/hook-external-links.php';
 				require_once EPL_PATH_LIB . 'hooks/hook-floorplan.php';
