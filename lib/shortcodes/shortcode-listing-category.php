095f092b3c70a606a056f52c0917cd6aacf5d8f6
Merv Barrett <mervb1@gmail.com>
2019-08-01 19:47:54 +0800

    Update shortcode-listing-category.php

--- lib/shortcodes/shortcode-listing-category.php
+++ lib/shortcodes/shortcode-listing-category.php
@@ -9,7 +9,7 @@
  * @since       1.1.1
  */
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -203,7 +203,7 @@ function epl_shortcode_listing_category_callback( $atts ) {
 	$query_open = new WP_Query( $args );
 	if ( $query_open->have_posts() ) { ?>
 		<div class="loop epl-shortcode">
-			<div class="loop-content epl-shortcode-listing-category 
+			<div class="loop-content epl-shortcode-listing-category
 			<?php
 			echo epl_template_class( $template, 'archive' );
 			echo $attributes['class'];
