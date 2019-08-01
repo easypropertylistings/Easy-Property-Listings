94859b791b20c205d1fc4b0f1bd0a55c6f51317d
Merv Barrett <mervb1@gmail.com>
2019-08-01 11:58:46 +0800

    Update extensions.php

--- lib/compatibility/extensions.php
+++ lib/compatibility/extensions.php
@@ -9,17 +9,19 @@
  * @since       1.0
  */
 
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-/*
- * Staff Directory Compatibility
+/**
+ * Staff Directory Compatibility.
  *
  * @since 1.0
  */
 function epl_sd_author_box_compat() {
-	if(has_action( 'epl_single_author' , 'epl_sd_advanced_author_box' ) || has_action( 'epl_single_author' , 'epl_sd_advanced_author_box_tabs' )) {
-		remove_action( 'epl_single_author','epl_property_author_box' );
+	if ( has_action( 'epl_single_author', 'epl_sd_advanced_author_box' ) || has_action( 'epl_single_author', 'epl_sd_advanced_author_box_tabs' ) ) {
+		remove_action( 'epl_single_author', 'epl_property_author_box' );
 	}
 }
-add_action( 'init' , 'epl_sd_author_box_compat' );
+add_action( 'init', 'epl_sd_author_box_compat' );
