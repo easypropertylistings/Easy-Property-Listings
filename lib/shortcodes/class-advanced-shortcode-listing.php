5a12e206fbf790a7cb2481336a0a78a54fcf5f2c
Merv Barrett <mervb1@gmail.com>
2019-08-01 18:23:52 +0800

    Update class-advanced-shortcode-listing.php

--- lib/shortcodes/class-advanced-shortcode-listing.php
+++ lib/shortcodes/class-advanced-shortcode-listing.php
@@ -10,7 +10,9 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 class EPL_Advanced_Shortcode_Listing {
 
@@ -24,10 +26,10 @@ class EPL_Advanced_Shortcode_Listing {
 
 	public $meta_key_price = 'property_price';
 
-	function __construct($atts,$overrides = array() ) {
+	function __construct( $atts, $overrides = array() ) {
 		$this->atts = $atts;
 		$this->shortcode_atts();
-		$this->override_atts($overrides);
+		$this->override_atts( $overrides );
 		$this->build_query();
 		$this->set_query();
 	}
@@ -37,12 +39,12 @@ class EPL_Advanced_Shortcode_Listing {
 	 *
 	 * @since 3.3
 	 */
-	function override_atts($overrides) {
+	function override_atts( $overrides ) {
 
-		if( !empty($overrides) ) {
+		if ( ! empty( $overrides ) ) {
 
-			foreach($overrides as $key 	=>	$value) {
-				$this->set_attribute($key,$value);
+			foreach ( $overrides as $key  => $value ) {
+				$this->set_attribute( $key, $value );
 			}
 		}
 	}
@@ -55,7 +57,7 @@ class EPL_Advanced_Shortcode_Listing {
 	function get_default_post_types() {
 
 		$property_types = epl_get_active_post_types();
-		if ( ! empty($property_types ) ) {
+		if ( ! empty( $property_types ) ) {
 			$property_types = array_keys( $property_types );
 		}
 
@@ -87,7 +89,7 @@ class EPL_Advanced_Shortcode_Listing {
 
 		/**
 		 *
-		 * Template 	: used to render each listing in loop.
+		 * Template     : used to render each listing in loop.
 		 *
 		 * Default to loop-listing-blog-default.php
 		 *
@@ -95,7 +97,7 @@ class EPL_Advanced_Shortcode_Listing {
 		 * active_theme/easypropertylistings and rename the file to loop-listing-blog-{$template}.php
 		 *
 		 *
-		 * wrap_template 	: Used to render wrapper of listings render using shortcode.
+		 * wrap_template    : Used to render wrapper of listings render using shortcode.
 		 *
 		 * Default to shortcode-listing.php
 		 *
@@ -105,33 +107,33 @@ class EPL_Advanced_Shortcode_Listing {
 		 */
 
 		$this->default_args = array(
-			'post_type'			=> $this->get_default_post_types(), //Post Type
-			'status'			=> array( 'current', 'sold', 'leased' ),
-			'commercial_listing_type'	=> '', // Listing Type, 'sale' , 'lease', 'both'
-			'feature'			=> '', // Feature slug
-			'feature_id'			=> '', // Feature ID
-			'limit'				=> '10', // Number of maximum posts to show
-			'offset'			=> '', // Offset posts. When used, pagination is disabled
-			'author'			=> '',	// Author of listings.
-			'agent'				=> '',	// listings by agent
-			'featured'			=> 0,	// Featured listings.
-			'open_house'			=> false, // only show open house
-			'auction'			=> false, // only show properties for auction
-			'class'				=> 'epl-shortcode-listing', // wrapper class
-			'wrap_template'			=> '', // explained in detail below
-			'template'			=> false, // Template can be set to "slim" for home open style template
-			'location'			=> '', // Location slug. Should be a name like sorrento
-			'location_id'			=> '', // Location ID
-			'tools_top'			=> 'off', // Tools before the loop like Sorter and Grid on or off
-			'tools_bottom'			=> 'off', // Tools after the loop like pagination on or off
-			'sortby'			=> '', // Options: price, date : Default date
-			'orderby_clause'		=> '', //  order by two different pieces of postmeta (for example, suburb first and state second)
-			'sort_order'			=> 'DESC', // Sort by ASC or DESC
-			'query_object'			=> '', // only for internal use . if provided use it instead of custom query
-			'pagination'			=> 'on', // Enable or disable pagination
-			'post__in'			=> '',	// show only these posts
-			'post__not_in'			=> '', // dont show these posts
-			'instance_id'			=> '1'
+			'post_type'               => $this->get_default_post_types(), // Post Type
+			'status'                  => array( 'current', 'sold', 'leased' ),
+			'commercial_listing_type' => '', // Listing Type, 'sale' , 'lease', 'both'
+			'feature'                 => '', // Feature slug
+			'feature_id'              => '', // Feature ID
+			'limit'                   => '10', // Number of maximum posts to show
+			'offset'                  => '', // Offset posts. When used, pagination is disabled
+			'author'                  => '',  // Author of listings.
+			'agent'                   => '',  // listings by agent
+			'featured'                => 0,   // Featured listings.
+			'open_house'              => false, // only show open house
+			'auction'                 => false, // only show properties for auction
+			'class'                   => 'epl-shortcode-listing', // wrapper class
+			'wrap_template'           => '', // explained in detail below
+			'template'                => false, // Template can be set to "slim" for home open style template
+			'location'                => '', // Location slug. Should be a name like sorrento
+			'location_id'             => '', // Location ID
+			'tools_top'               => 'off', // Tools before the loop like Sorter and Grid on or off
+			'tools_bottom'            => 'off', // Tools after the loop like pagination on or off
+			'sortby'                  => '', // Options: price, date : Default date
+			'orderby_clause'          => '', // order by two different pieces of postmeta (for example, suburb first and state second)
+			'sort_order'              => 'DESC', // Sort by ASC or DESC
+			'query_object'            => '', // only for internal use . if provided use it instead of custom query
+			'pagination'              => 'on', // Enable or disable pagination
+			'post__in'                => '',  // show only these posts
+			'post__not_in'            => '', // dont show these posts
+			'instance_id'             => '1',
 		);
 
 		return $this->default_args;
@@ -151,10 +153,10 @@ class EPL_Advanced_Shortcode_Listing {
 	 *
 	 * @since 3.3
 	 */
-	function set_attribute($key,$value) {
+	function set_attribute( $key, $value ) {
 
-		if( isset($this->attributes[$key]) ) {
-			$this->attributes[$key] = $value;
+		if ( isset( $this->attributes[ $key ] ) ) {
+			$this->attributes[ $key ] = $value;
 		}
 	}
 
@@ -163,9 +165,9 @@ class EPL_Advanced_Shortcode_Listing {
 	 *
 	 * @since 3.3
 	 */
-	function get_attribute($key) {
+	function get_attribute( $key ) {
 
-		return isset($this->attributes[$key]) ? $this->attributes[$key] : null;
+		return isset( $this->attributes[ $key ] ) ? $this->attributes[ $key ] : null;
 	}
 
 	/**
@@ -176,7 +178,7 @@ class EPL_Advanced_Shortcode_Listing {
 	function set_post_type() {
 		if ( ! is_array( $this->attributes['post_type'] ) ) {
 			$this->attributes['post_type'] =
-			array_map( 'trim', explode( ',',$this->attributes['post_type'] ) );
+			array_map( 'trim', explode( ',', $this->attributes['post_type'] ) );
 		}
 	}
 
@@ -187,11 +189,11 @@ class EPL_Advanced_Shortcode_Listing {
 	 */
 	function set_initial_args() {
 
-		$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+		$paged      = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 		$this->args = array(
-			'post_type'      =>	$this->attributes['post_type'],
-			'posts_per_page' =>	$this->attributes['limit'],
-			'paged'          =>	absint( $paged ),
+			'post_type'      => $this->attributes['post_type'],
+			'posts_per_page' => $this->attributes['limit'],
+			'paged'          => absint( $paged ),
 		);
 	}
 
@@ -203,9 +205,9 @@ class EPL_Advanced_Shortcode_Listing {
 	function set_offset() {
 
 		// Offset query does not work with pagination
-		if ( ! empty ( $this->attributes['offset'] ) ) {
-			$this->args['offset'] 		= $this->attributes['offset'];
-			$this->attributes['pagination'] 	= 'off'; // Disable pagination when offset is used
+		if ( ! empty( $this->attributes['offset'] ) ) {
+			$this->args['offset']           = $this->attributes['offset'];
+			$this->attributes['pagination'] = 'off'; // Disable pagination when offset is used
 		}
 	}
 
@@ -216,8 +218,8 @@ class EPL_Advanced_Shortcode_Listing {
 	 */
 	function set_post__in() {
 		if ( ! empty( $this->attributes['post__in'] ) ) {
-			$post__in = array_map( 'trim', explode( ',',$this->attributes['post__in'] ) );
-			if( !empty($post__in) ){
+			$post__in = array_map( 'trim', explode( ',', $this->attributes['post__in'] ) );
+			if ( ! empty( $post__in ) ) {
 				$this->args['post__in'] = $post__in;
 			}
 		}
@@ -230,8 +232,8 @@ class EPL_Advanced_Shortcode_Listing {
 	 */
 	function set_post__not_in() {
 		if ( ! empty( $this->attributes['post__not_in'] ) ) {
-			$post__not_in = array_map( 'trim', explode( ',',$this->attributes['post__not_in'] ) );
-			if( !empty($post__not_in) ){
+			$post__not_in = array_map( 'trim', explode( ',', $this->attributes['post__not_in'] ) );
+			if ( ! empty( $post__not_in ) ) {
 				$this->args['post__not_in'] = $post__not_in;
 			}
 		}
@@ -244,7 +246,7 @@ class EPL_Advanced_Shortcode_Listing {
 	 */
 	function process_epl_atts() {
 
-		$this->args['meta_query'] = epl_parse_atts($this->atts);
+		$this->args['meta_query'] = epl_parse_atts( $this->atts );
 	}
 
 	/**
@@ -258,7 +260,7 @@ class EPL_Advanced_Shortcode_Listing {
 		if ( ! empty( $this->attributes['author'] ) ) {
 			$this->attributes['author'] = (array) $this->attributes['author'];
 			if ( is_array( $this->attributes['author'] ) ) {
-				$author_ids = array_map( 'epl_get_author_id_from_name', $this->attributes['author'] );
+				$author_ids                 = array_map( 'epl_get_author_id_from_name', $this->attributes['author'] );
 				$this->attributes['author'] = implode( ',', $author_ids );
 			}
 			$this->args['author'] = trim( $this->attributes['author'] );
@@ -275,23 +277,23 @@ class EPL_Advanced_Shortcode_Listing {
 		// Listings by specified agent.
 		if ( ! empty( $this->attributes['agent'] ) ) {
 			$this->attributes['agent'] = array_map( 'trim', explode( ',', $this->attributes['agent'] ) );
-			$this->attributes['agent'] = array_filter($this->attributes['agent']);
+			$this->attributes['agent'] = array_filter( $this->attributes['agent'] );
 
 			$agent_meta_query = array(
-				'relation'  =>  'OR',
+				'relation' => 'OR',
 			);
 
-			foreach($this->attributes['agent'] as $single_agent) {
+			foreach ( $this->attributes['agent'] as $single_agent ) {
 				$agent_meta_query[] = array(
-	                'key'       =>  'property_agent',
-	                'value'     =>  array($single_agent, sanitize_user($single_agent) ),
-	                'compare'   =>  'IN'
-	            );
-	            $agent_meta_query[] = array(
-	                'key'       =>  'property_second_agent',
-	                'value'     =>  array($single_agent, sanitize_user($single_agent) ),
-	                'compare'   =>  'IN'
-	            );
+					'key'     => 'property_agent',
+					'value'   => array( $single_agent, sanitize_user( $single_agent ) ),
+					'compare' => 'IN',
+				);
+				$agent_meta_query[] = array(
+					'key'     => 'property_second_agent',
+					'value'   => array( $single_agent, sanitize_user( $single_agent ) ),
+					'compare' => 'IN',
+				);
 			}
 
 			$this->args['meta_query']['agent_clause'] = $agent_meta_query;
@@ -324,8 +326,8 @@ class EPL_Advanced_Shortcode_Listing {
 		// auction only listings
 		if ( $this->attributes['auction'] ) {
 			$this->args['meta_query']['property_authority_clause'] = array(
-				'key' 		=> 'property_authority',
-				'value' 	=> 'auction'
+				'key'   => 'property_authority',
+				'value' => 'auction',
 			);
 		}
 	}
@@ -340,9 +342,9 @@ class EPL_Advanced_Shortcode_Listing {
 		// open house only ?
 		if ( $this->attributes['open_house'] ) {
 			$this->args['meta_query']['open_house_clause'] = array(
-				'key' 		=> 'property_inspection_times',
-				'value' 	=> '',
-				'compare' 	=> '!=',
+				'key'     => 'property_inspection_times',
+				'value'   => '',
+				'compare' => '!=',
 			);
 		}
 	}
@@ -362,9 +364,9 @@ class EPL_Advanced_Shortcode_Listing {
 				$this->attributes['location'] = array_map( 'trim', explode( ',', $this->attributes['location'] ) );
 
 				$this->args['tax_query']['tax_location_clause'] = array(
-					'taxonomy'	=> 'location',
-					'field'		=> 'slug',
-					'terms' 	=> $this->attributes['location'],
+					'taxonomy' => 'location',
+					'field'    => 'slug',
+					'terms'    => $this->attributes['location'],
 				);
 			}
 		}
@@ -375,9 +377,9 @@ class EPL_Advanced_Shortcode_Listing {
 				$this->attributes['location_id'] = array_map( 'trim', explode( ',', $this->attributes['location_id'] ) );
 
 				$this->args['tax_query']['tax_location_id_clause'] = array(
-					'taxonomy'	=> 'location',
-					'field'		=> 'id',
-					'terms' 	=> $this->attributes['location_id'],
+					'taxonomy' => 'location',
+					'field'    => 'id',
+					'terms'    => $this->attributes['location_id'],
 				);
 			}
 		}
@@ -398,9 +400,9 @@ class EPL_Advanced_Shortcode_Listing {
 				$this->attributes['feature'] = array_map( 'trim', explode( ',', $this->attributes['feature'] ) );
 
 				$this->args['tax_query']['tax_feature_clause'] = array(
-					'taxonomy'	=> 'tax_feature',
-					'field'		=> 'slug',
-					'terms' 	=> $this->attributes['feature'],
+					'taxonomy' => 'tax_feature',
+					'field'    => 'slug',
+					'terms'    => $this->attributes['feature'],
 				);
 			}
 		}
@@ -410,9 +412,9 @@ class EPL_Advanced_Shortcode_Listing {
 				$this->attributes['feature_id'] = array_map( 'trim', explode( ',', $this->attributes['feature_id'] ) );
 
 				$this->args['tax_query']['tax_feature_id_clause'] = array(
-					'taxonomy'	=> 'tax_feature',
-					'field'		=> 'id',
-					'terms' 	=> $this->attributes['feature_id'],
+					'taxonomy' => 'tax_feature',
+					'field'    => 'id',
+					'terms'    => $this->attributes['feature_id'],
 				);
 			}
 		}
@@ -430,9 +432,9 @@ class EPL_Advanced_Shortcode_Listing {
 				$this->attributes['status'] = array_map( 'trim', explode( ',', $this->attributes['status'] ) );
 
 				$this->args['meta_query']['property_status_clause'] = array(
-					'key'		=> 'property_status',
-					'value'		=> $this->attributes['status'],
-					'compare'	=> 'IN',
+					'key'     => 'property_status',
+					'value'   => $this->attributes['status'],
+					'compare' => 'IN',
 				);
 
 				add_filter( 'epl_sorting_options', 'epl_sorting_options_callback' );
@@ -453,9 +455,9 @@ class EPL_Advanced_Shortcode_Listing {
 				$this->attributes['commercial_listing_type'] = array_map( 'trim', explode( ',', $this->attributes['commercial_listing_type'] ) );
 
 				$this->args['meta_query']['property_com_listing_type_clause'] = array(
-					'key'		=> 'property_com_listing_type',
-					'value'		=> $this->attributes['commercial_listing_type'],
-					'compare'	=> 'IN',
+					'key'     => 'property_com_listing_type',
+					'value'   => $this->attributes['commercial_listing_type'],
+					'compare' => 'IN',
 				);
 			}
 		}
@@ -468,20 +470,20 @@ class EPL_Advanced_Shortcode_Listing {
 	 */
 	function set_orderby() {
 
-		if ( ! empty ( $this->attributes['sortby'] ) ) {
+		if ( ! empty( $this->attributes['sortby'] ) ) {
 			if ( $this->attributes['sortby'] == 'price' ) {
 				$this->args['orderby']  = 'meta_value_num';
-				$this->args['meta_key'] =	$this->get_meta_key_price();
-			} elseif( $this->attributes['sortby'] == 'rand' ) {
-				$this->args['orderby']  = 'rand';
+				$this->args['meta_key'] = $this->get_meta_key_price();
+			} elseif ( $this->attributes['sortby'] == 'rand' ) {
+				$this->args['orderby'] = 'rand';
 			} elseif ( $this->attributes['sortby'] == 'status' ) {
 				$this->args['orderby']  = 'meta_value';
 				$this->args['meta_key'] = 'property_status';
 			} else {
-				$this->args['orderby']  = 'post_date';
-				$this->args['order']    = 'DESC';
+				$this->args['orderby'] = 'post_date';
+				$this->args['order']   = 'DESC';
 			}
-			$this->args['order']        = $this->attributes['sort_order'];
+			$this->args['order'] = $this->attributes['sort_order'];
 		}
 	}
 
@@ -497,26 +499,26 @@ class EPL_Advanced_Shortcode_Listing {
 		 * will override default orderby
 		 */
 
-		if ( ! empty ( $this->attributes['orderby_clause'] ) ) {
+		if ( ! empty( $this->attributes['orderby_clause'] ) ) {
 
 			$this->attributes['orderby_clause'] =
-			array_map( 'trim', explode( ',',$this->attributes['orderby_clause'] ) );
-			foreach( $this->attributes['orderby_clause'] as $oc ) {
-				$oc_array = explode( '|',$oc );
+			array_map( 'trim', explode( ',', $this->attributes['orderby_clause'] ) );
+			foreach ( $this->attributes['orderby_clause'] as $oc ) {
+				$oc_array = explode( '|', $oc );
 
-				$this->args['orderby'][$oc_array[0]] = $oc_array[1];
+				$this->args['orderby'][ $oc_array[0] ] = $oc_array[1];
 
 			}
 
-			unset($this->args['order']);
+			unset( $this->args['order'] );
 		}
 
 		$args['instance_id'] = $this->attributes['instance_id'];
 		// add sortby arguments to query, if listings sorted by $_GET['sortby'];
-		$args = epl_add_orderby_args($args,'shortcode','listing_advanced');
+		$args = epl_add_orderby_args( $args, 'shortcode', 'listing_advanced' );
 
 		/** Option to filter args */
-		$args = apply_filters('epl_shortcode_listing_advanced_args',$this->args,$this->attributes);
+		$args = apply_filters( 'epl_shortcode_listing_advanced_args', $this->args, $this->attributes );
 	}
 
 	/**
@@ -544,7 +546,7 @@ class EPL_Advanced_Shortcode_Listing {
 
 		$wrap_template =
 		$this->attributes['wrap_template'] == '' ?
-		'shortcode-listing.php' : 'shortcode-listing-'.$this->attributes['wrap_template'].'.php';
+		'shortcode-listing.php' : 'shortcode-listing-' . $this->attributes['wrap_template'] . '.php';
 
 		return $wrap_template;
 	}
