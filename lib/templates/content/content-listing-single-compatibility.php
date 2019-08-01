f67d844c1c083c683cbb9db0d8889e816aa4688e
Merv Barrett <mervb1@gmail.com>
2019-08-01 22:25:05 +0800

    Update content-listing-single-compatibility.php

--- lib/templates/content/content-listing-single-compatibility.php
+++ lib/templates/content/content-listing-single-compatibility.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Single Property Template: Expanded Compatibility
  *
  * @package     EPL
@@ -9,8 +9,10 @@
  * @since       1.0
  */
 
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 ?>
 
@@ -24,18 +26,18 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 		<div class="epl-tab-section epl-section-property-details">
 			<div class="tab-content">
 
-				<?php do_action('epl_property_price_before'); ?>
+				<?php do_action( 'epl_property_price_before' ); ?>
 				<div class="property-meta pricing-compatibility">
-					<?php do_action('epl_property_price'); ?>
+					<?php do_action( 'epl_property_price' ); ?>
 				</div>
-				<?php do_action('epl_property_price_after'); ?>
+				<?php do_action( 'epl_property_price_after' ); ?>
 				<div class="property-feature-icons epl-clearfix">
-					<?php do_action('epl_property_icons'); ?>
+					<?php do_action( 'epl_property_icons' ); ?>
 				</div>
-				<?php do_action('epl_property_land_category'); ?>
-				<?php do_action('epl_property_commercial_category'); ?>
-				<?php do_action('epl_property_available_dates');// meant for rent only ?>
-				<?php do_action('epl_property_inspection_times'); ?>
+				<?php do_action( 'epl_property_land_category' ); ?>
+				<?php do_action( 'epl_property_commercial_category' ); ?>
+				<?php do_action( 'epl_property_available_dates' ); // meant for rent only. ?>
+				<?php do_action( 'epl_property_inspection_times' ); ?>
 			</div>
 		</div>
 	</div>
@@ -45,29 +47,29 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 		<div class="epl-tab-wrapper tab-wrapper">
 
 			<div class="epl-tab-section epl-section-description">
-				<h5 class="epl-tab-title"><?php echo apply_filters('epl_property_tab_title_description',__('Description', 'easy-property-listings' )); ?></h5>
+				<h5 class="epl-tab-title"><?php echo apply_filters( 'epl_property_tab_title_description', __( 'Description', 'easy-property-listings' ) ); ?></h5>
 				<div class="tab-content">
 					<!-- heading -->
-					<h2 class="entry-title"><?php do_action('epl_property_heading'); ?></h2>
+					<h2 class="entry-title"><?php do_action( 'epl_property_heading' ); ?></h2>
 
-					<h3 class="secondary-heading"><?php do_action('epl_property_secondary_heading'); ?></h3>
+					<h3 class="secondary-heading"><?php do_action( 'epl_property_secondary_heading' ); ?></h3>
 					<?php
-						do_action('epl_property_content_before');
+						do_action( 'epl_property_content_before' );
 
-						do_action('epl_property_the_content');
+						do_action( 'epl_property_the_content' );
 
-						do_action('epl_property_content_after'); // For Extension Support
+						do_action( 'epl_property_content_after' ); // For Extension Support.
 
-						//do_action('epl_property_video');
+						// do_action('epl_property_video');.
 					?>
 				</div>
 			</div>
 
-			<?php do_action('epl_property_tab_section_before'); ?>
+			<?php do_action( 'epl_property_tab_section_before' ); ?>
 			<div class="epl-tab-section epl-tab-section-features">
-				<?php do_action('epl_property_tab_section'); ?>
+				<?php do_action( 'epl_property_tab_section' ); ?>
 			</div>
-			<?php do_action('epl_property_tab_section_after'); ?>
+			<?php do_action( 'epl_property_tab_section_after' ); ?>
 
 			<?php do_action( 'epl_property_gallery' ); ?>
 
@@ -83,7 +85,15 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	<!-- categories, tags and comments -->
 	<div class="entry-footer epl-clearfix">
 		<div class="entry-meta">
-			<?php wp_link_pages( array( 'before' => '<div class="entry-utility entry-pages">' . __( 'Pages:', 'easy-property-listings'  ) . '', 'after' => '</div>', 'next_or_number' => 'number' ) ); ?>
+			<?php
+			wp_link_pages(
+				array(
+					'before'         => '<div class="entry-utility entry-pages">' . __( 'Pages:', 'easy-property-listings' ) . '',
+					'after'          => '</div>',
+					'next_or_number' => 'number',
+				)
+			);
+			?>
 		</div>
 	</div>
 </div>
