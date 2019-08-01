014c0323c845878cf6546c36ecacb2ce5280932f
Merv Barrett <mervb1@gmail.com>
2019-08-01 22:32:06 +0800

    Update loop-listing-blog-slim.php

--- lib/templates/content/loop-listing-blog-slim.php
+++ lib/templates/content/loop-listing-blog-slim.php
@@ -10,12 +10,14 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 ?>
 
-<div id="post-<?php the_ID(); ?>" <?php post_class('epl-listing-post epl-property-blog epl-property-blog-slim epl-clearfix'); ?>>
+<div id="post-<?php the_ID(); ?>" <?php post_class( 'epl-listing-post epl-property-blog epl-property-blog-slim epl-clearfix' ); ?>>
 	<div class="epl-property-blog-entry-wrapper epl-clearfix">
-		<?php do_action('epl_property_loop_before_content'); ?>
+		<?php do_action( 'epl_property_loop_before_content' ); ?>
 			<?php if ( has_post_thumbnail() ) : ?>
 				<div class="property-box slim property-box-left property-box-left-slim property-featured-image-wrapper">
 					<a href="<?php the_permalink(); ?>">
@@ -26,29 +28,27 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 			<div class="property-box slim property-box-right property-box-right-slim property-content">
 				<!-- Heading -->
-				<h3 class="entry-title"><a href="<?php the_permalink() ?>"><?php do_action('epl_property_heading'); ?></a></h3>
-
-				<?php //the_excerpt(); ?>
+				<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php do_action( 'epl_property_heading' ); ?></a></h3>
 
 			<!-- Address -->
 			<div class="property-address">
 				<a href="<?php the_permalink(); ?>">
-					<?php do_action('epl_property_tab_address'); ?>
+					<?php do_action( 'epl_property_tab_address' ); ?>
 				</a>
 			</div>
 
 				<!-- Home Open -->
-				<?php do_action('epl_property_inspection_times'); ?>
+				<?php do_action( 'epl_property_inspection_times' ); ?>
 
 				<!-- Property Featured Icons -->
 				<div class="property-feature-icons">
-					<?php do_action('epl_property_icons'); ?>
+					<?php do_action( 'epl_property_icons' ); ?>
 				</div>
 
 				<div class="price">
-					<?php do_action('epl_property_price'); ?>
+					<?php do_action( 'epl_property_price' ); ?>
 				</div>
 			</div>
-		<?php do_action('epl_property_loop_after_content'); ?>
+		<?php do_action( 'epl_property_loop_after_content' ); ?>
 	</div>
 </div>
