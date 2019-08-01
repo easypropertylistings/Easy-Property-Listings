16fa70f99b25ed0cd8aecf9d1660b3f029cf5cfa
Merv Barrett <mervb1@gmail.com>
2019-08-01 22:32:30 +0800

    Update loop-listing-blog-table-open.php

--- lib/templates/content/loop-listing-blog-table-open.php
+++ lib/templates/content/loop-listing-blog-table-open.php
@@ -10,13 +10,15 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 global $property;
 ?>
 
-<div id="post-<?php the_ID(); ?>" <?php post_class('epl-listing-post epl-property-blog epl-property-table epl-table epl-table-open epl-clearfix'); ?>>
-	<?php do_action('epl_property_before_content'); ?>
+<div id="post-<?php the_ID(); ?>" <?php post_class( 'epl-listing-post epl-property-blog epl-property-table epl-table epl-table-open epl-clearfix' ); ?>>
+	<?php do_action( 'epl_property_before_content' ); ?>
 		<div class="epl-table-column-image property-featured-image-wrapper">
 			<a href="<?php the_permalink(); ?>">
 				<div class="epl-blog-image">
@@ -29,20 +31,20 @@ global $property;
 			<!-- Address -->
 			<div class="epl-table-box epl-table-column epl-table-column-left">
 				<div class="epl-table-address property-address">
-					<a href="<?php the_permalink(); ?>"><?php do_action('epl_property_address'); ?></a>
+					<a href="<?php the_permalink(); ?>"><?php do_action( 'epl_property_address' ); ?></a>
 				</div>
 				<div class="property-feature-icons">
-					<?php do_action('epl_property_icons'); ?>
+					<?php do_action( 'epl_property_icons' ); ?>
 				</div>
 			</div>
 			<!-- Property Featured Icons -->
 			<div class="epl-table-box epl-table-column epl-table-column-middle price">
-				<?php do_action('epl_property_price'); ?>
+				<?php do_action( 'epl_property_price' ); ?>
 			</div>
 			<!-- Price -->
 			<div class="epl-table-box epl-table-column epl-table-column-right">
-				<?php do_action('epl_property_inspection_times'); ?>
+				<?php do_action( 'epl_property_inspection_times' ); ?>
 			</div>
 		</div>
-	<?php do_action('epl_property_after_content'); ?>
+	<?php do_action( 'epl_property_after_content' ); ?>
 </div>
