c62a6d6facec365e2fe8c962c4b3ebeedc0b6158
Merv Barrett <mervb1@gmail.com>
2019-08-01 21:02:36 +0800

    renamed file

--- easy-property-listings.php
+++ easy-property-listings.php
@@ -240,7 +240,7 @@ if ( ! class_exists( 'Easy_Property_Listings' ) ) :
 			require_once EPL_PATH_LIB . 'widgets/widget-listing.php';
 			require_once EPL_PATH_LIB . 'widgets/widget-listing-gallery.php';
 			require_once EPL_PATH_LIB . 'widgets/widget-listing-search.php';
-			require_once EPL_PATH_LIB . 'widgets/widget-contact-capture.php';
+			require_once EPL_PATH_LIB . 'widgets/class-epl-widget-contact-capture.php';
 
 			require_once EPL_PATH_LIB . 'includes/class-epl-property-meta.php';
 			require_once EPL_PATH_LIB . 'includes/class-epl-author-meta.php';
