9a376fdb0e8122ed3e52e870142b48891df13d5f
Merv Barrett <mervb1@gmail.com>
2019-08-01 22:12:29 +0800

    Update content-author-archive-card.php

--- lib/templates/content/content-author-archive-card.php
+++ lib/templates/content/content-author-archive-card.php
@@ -10,24 +10,26 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 ?>
 
 <div class="epl-loop-author-box-wrapper">
 
 	<div class="epl-loop-author-image">
-		<?php do_action('epl_author_thumbnail',$epl_author); ?>
+		<?php do_action( 'epl_author_thumbnail', $epl_author ); ?>
 	</div>
 	<div class="epl-loop-author-content">
 		<div class="epl-loop-author-title">
 			<?php
-				$permalink 	= apply_filters('epl_author_profile_link', get_author_posts_url($epl_author->author_id) , $epl_author);
+				$permalink = apply_filters( 'epl_author_profile_link', get_author_posts_url( $epl_author->author_id ), $epl_author );
 
-				$author_title =  apply_filters('epl_author_profile_title',get_the_author_meta( 'display_name',$epl_author->author_id ) ,$epl_author );
+				$author_title = apply_filters( 'epl_author_profile_title', get_the_author_meta( 'display_name', $epl_author->author_id ), $epl_author );
 			?>
-			<a href="<?php echo $permalink ?>">
-				<?php echo $author_title;  ?>
+			<a href="<?php echo $permalink; ?>">
+				<?php echo $author_title; ?>
 			</a>
 		</div>
 	</div>
