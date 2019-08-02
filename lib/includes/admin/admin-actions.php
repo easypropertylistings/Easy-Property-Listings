827405284e8a821413a0322325326d036807fc6d
Merv Barrett <mervb1@gmail.com>
2019-08-02 14:12:59 +0800

    Update admin-actions.php

--- lib/includes/admin/admin-actions.php
+++ lib/includes/admin/admin-actions.php
@@ -6,10 +6,10 @@
  * @subpackage  Admin/Actions
  * @copyright   Copyright (c) 2019, Merv Barrett
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       3.0
+ * @since       3.0.0
  */
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -17,20 +17,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Renders map in address meta box
  *
- * @since 3.0
+ * @since 3.0.0
+ * @param string $address The listing address.
  * @return void
  */
 function epl_admin_listing_map( $address ) { ?>
 	<div id='epl_admin_map_canvas' data-address="<?php echo $address; ?>"></div>
-															<?php
+	<?php
 }
 add_action( 'epl_admin_listing_map', 'epl_admin_listing_map' );
 
 /**
  * Add a class or many to the body in the dashboard
  *
- * @since 3.0
- * @return void
+ * @since 3.0.0
+ * @param string $classes Classes.
+ * @return string of classes.
  */
 function epl_add_admin_body_class( $classes ) {
 
@@ -46,15 +48,15 @@ add_filter( 'admin_body_class', 'epl_add_admin_body_class' );
  * Google Maps API Key Notification
  *
  * @since 3.1.5
- * @return admin notice text
+ * @return void
  */
 function epl_admin_google_maps_api_notification() {
 
 	$screen = get_current_screen();
 
-	if ( $screen->id == 'easy-property-listings_page_epl-settings' ) {
+	if ( 'easy-property-listings_page_epl-settings' === $screen->id ) {
 
-		if ( current_user_can( 'manage_options' ) && epl_get_option( 'epl_disable_google_api' ) != 'on' && epl_get_option( 'epl_google_api_key' ) == '' ) {
+		if ( current_user_can( 'manage_options' ) && 'on' !== epl_get_option( 'epl_disable_google_api' ) && '' === epl_get_option( 'epl_google_api_key' ) ) {
 			?>
 			<div class="notice notice-error is-dismissible">
 				<h3 class="epl-text-red" style="color:#dd3d36"><?php _e( 'Easy Property Listings requires a Google API key for mapping', 'easy-property-listings' ); ?></h3>
@@ -62,10 +64,17 @@ function epl_admin_google_maps_api_notification() {
 				<p><strong><?php _e( 'As of June 22, 2016 Google has made API keys required for the Javascript Maps API.', 'easy-property-listings' ); ?></strong><br>
 
 					<?php $link = '<strong><a href="https://developers.google.com/maps/documentation/javascript/get-api-key" target="_blank">' . __( 'Google Maps API Key', 'easy-property-listings' ) . '</a></strong>'; ?>
-					<?php printf( __( 'To allow maps to function correctly please create a %s and enable <strong>Google Maps API</strong>.', 'easy-property-listings' ), $link ); ?><br>
+					<?php
+						/* Translators: %s is a link. */
+						printf( __( 'To allow maps to function correctly please create a %s and enable <strong>Google Maps API</strong>.', 'easy-property-listings' ), $link );
+					?>
+					<br>
 
 					<?php $link = '<strong><a href="#epl-advanced">' . __( 'Advanced Settings', 'easy-property-listings' ) . '</a></strong>'; ?>
-					<?php printf( __( 'Once complete enter your API key below under %s > Google Maps API Key. When set, refresh page to dismiss notice.', 'easy-property-listings' ), $link ); ?>
+					<?php
+						/* Translators: %s is a link to advanced settings. */
+						printf( __( 'Once complete enter your API key below under %s > Google Maps API Key. When set, refresh page to dismiss notice.', 'easy-property-listings' ), $link );
+					?>
 				</p>
 			</div>
 			<?php
