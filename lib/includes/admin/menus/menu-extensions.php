c0c29ae2991cd8bee1963cd260f4a29ed119a9fd
Merv Barrett <mervb1@gmail.com>
2019-08-02 17:45:33 +0800

    Update menu-extensions.php

--- lib/includes/admin/menus/menu-extensions.php
+++ lib/includes/admin/menus/menu-extensions.php
@@ -33,7 +33,7 @@ if ( isset( $_REQUEST['action'] ) && 'epl_settings' === $_REQUEST['action'] ) {
 			$ext_field_groups = $epl_extensions[ $active_tab ];
 			foreach ( $ext_field_groups['fields'] as $ext_field_group ) {
 				foreach ( $ext_field_group['fields'] as $field ) {
-					if ( $field['type'] == 'radio' || $field['type'] == 'checkbox' ) {
+					if ( 'radio' === $field['type'] || 'checkbox' === $field['type'] ) {
 
 						if ( ! isset( $_REQUEST[ $field['name'] ] ) ) {
 							$_REQUEST[ $field['name'] ] = '';
@@ -42,7 +42,7 @@ if ( isset( $_REQUEST['action'] ) && 'epl_settings' === $_REQUEST['action'] ) {
 						}
 					}
 
-					if ( $field['type'] == 'text' ) {
+					if ( 'text' === $field['type'] ) {
 
 						if ( isset( $_REQUEST[ $field['name'] ] ) && is_array( $_REQUEST[ $field['name'] ] ) ) {
 							array_walk_recursive( $_REQUEST[ $field['name'] ], 'sanitize_text_field' );
@@ -56,31 +56,27 @@ if ( isset( $_REQUEST['action'] ) && 'epl_settings' === $_REQUEST['action'] ) {
 					if ( isset( $_REQUEST[ $field['name'] ] ) ) {
 						$epl_settings[ $field['name'] ] = $_REQUEST[ $field['name'] ];
 
-						// remove fields after adding them to epl_settings
+						// Remove fields after adding them to epl_settings.
 						unset( $_REQUEST[ $field['name'] ] );
 					}
 				}
 			}
 
-			/**
-			* adding some way for dynamic fields to be added to epl_settings array
-			*/
+			// Adding some way for dynamic fields to be added to epl_settings array.
 
-			/** settings page params */
+			// Settings page params.
 			$page_params = apply_filters( 'epl_settings_page_params', array( 'tab', 'page', 'sub_tab', 'action', 'submit' ) );
 
-			/** remove all settings page params from $_REQUEST */
+			// Remove all settings page params from $_REQUEST.
 			foreach ( $page_params as $page_param ) {
 				if ( isset( $_REQUEST[ $page_param ] ) ) {
 					unset( $_REQUEST[ $page_param ] );
 				}
 			}
 
-			/** remain fields are probably dynamic fields added via javascript/jquery
-			 *  save them as well if they are not already in $epl_settings
-			 */
+			// Remaining fields are probably dynamic fields added via javascript/jquery save them as well if they are not already in $epl_settings.
 			foreach ( $_REQUEST as $dynamic_key  => $dynamic_value ) {
-				if ( $dynamic_value != '' ) {
+				if ( '' !== $dynamic_value ) {
 					$epl_settings[ $dynamic_key ] = $dynamic_value;
 				}
 			}
@@ -99,39 +95,34 @@ $epl_settings = get_option( 'epl_settings' );
 	<h2 class="nav-tab-wrapper epl-nav-tab-wrapper">
 		<?php
 		foreach ( $epl_extensions as $ext_key => $epl_extension ) {
-			$nav_active = $active_tab == $ext_key ? 'nav-tab-active' : '';
+			$nav_active = $active_tab === $ext_key ? 'nav-tab-active' : '';
 			?>
 				<a class="nav-tab <?php echo $nav_active; ?>"
 					href="<?php echo admin_url( 'admin.php?page=epl-extensions&tab=' . sanitize_title( $ext_key ) ); ?>">
-					<?php _e( $epl_extension['label'], 'easy-property-listings' ); ?>
+					<?php echo $epl_extension['label']; ?>
 				</a>
 				<?php
 		}
 		?>
-
 	</h2>
 	<?php
-	/** re get fields so that we can get updated fields if added via hook **/
+	// Get fields so that we can get updated fields if added via hook.
 	$epl_extensions = epl_get_new_admin_option_fields();
 	if ( array_key_exists( $active_tab, $epl_extensions ) ) :
 		$ext_field_groups = $epl_extensions[ $active_tab ];
 		?>
 		<div class="epl-content">
 			<?php
-			echo '
-				<div class="epl-fields-help-content">';
+			echo '<div class="epl-fields-help-content">';
 			foreach ( $ext_field_groups['fields'] as $ext_field_group ) {
 
 				if ( isset( $ext_field_group['intro'] ) && ! empty( $ext_field_group['intro'] ) ) {
 					echo '<div class="epl-field epl-field-intro" data-help="tab-menu-' . sanitize_title( $ext_field_group['label'] ) . '">';
-					_e( $ext_field_group['intro'], 'easy-property-listings' );
+					echo $ext_field_group['intro'];
 					echo '</div>';
 				}
 			}
-						echo '
-				</div>
-			';
-
+			echo '</div>';
 			?>
 
 			<form action="" method="post" id="epl_extension_settings_form">
@@ -143,14 +134,14 @@ $epl_settings = get_option( 'epl_settings' );
 						<ul>';
 				foreach ( $ext_field_groups['fields'] as $ext_field_group ) {
 
-					$current_class = $active_sub_tab == 'tab-menu-' . sanitize_title( $ext_field_group['label'] ) ? 'epl-fields-menu-current' : '';
-					if ( $active_sub_tab == '' && $counter == 1 ) {
+					$current_class = 'tab-menu-' === $active_sub_tab . sanitize_title( $ext_field_group['label'] ) ? 'epl-fields-menu-current' : '';
+					if ( '' === $active_sub_tab && 1 === $counter ) {
 						$current_class = 'epl-fields-menu-current';
 					}
 					if ( ! empty( $ext_field_group['label'] ) ) {
 						?>
 									<li class="<?php echo $current_class; ?>" data-tab="<?php echo 'tab-menu-' . sanitize_title( $ext_field_group['label'] ); ?>">
-										<?php _e( $ext_field_group['label'], 'easy-property-listings' ); ?>
+										<?php echo $ext_field_group['label']; ?>
 									</li>
 									<?php
 					}
@@ -166,47 +157,39 @@ $epl_settings = get_option( 'epl_settings' );
 					echo '<div class="epl-fields-tab-content">';
 					$counter = 1;
 					foreach ( $ext_field_groups['fields'] as $field_group ) {
-						$current_class = $active_sub_tab == 'tab-menu-' . sanitize_title( $field_group['label'] ) ? 'epl-fields-field-current' : '';
-						if ( $active_sub_tab == '' && $counter == 1 ) {
+						$current_class = 'tab-menu-' === $active_sub_tab . sanitize_title( $field_group['label'] ) ? 'epl-fields-field-current' : '';
+						if ( '' === $active_sub_tab && 1 === $counter ) {
 							$current_class = 'epl-fields-field-current';
 						}
 						?>
 
-
 						<div class="<?php echo $current_class; ?> epl-fields-single-menu" id="<?php echo 'tab-menu-' . sanitize_title( $field_group['label'] ); ?>">
-											   <?php
-
-												foreach ( $field_group['fields'] as $field ) {
-													?>
+							<?php foreach ( $field_group['fields'] as $field ) { ?>
 							<div class="epl-field">
 								<div class="epl-help-entry-header">
-									<div class="epl_help_entry_content<?php // echo $field['name']; ?>"><?php // _e($field['help'], 'easy-property-listings' ); ?></div>
+									<div class="epl_help_entry_content<?php // echo $field['name'];. ?>"><?php // _e($field['help'], 'easy-property-listings' );. ?></div>
 								</div>
-													<?php if ( $field['type'] != 'help' ) { ?>
+								<?php if ( 'help' !== $field['type'] ) { ?>
 									<div class="epl-half-left">
-										<label for="<?php echo $field['name']; ?>"><?php _e( $field['label'], 'easy-property-listings' ); ?></label>
-
+										<label for="<?php echo $field['name']; ?>"><?php echo $field['label']; ?></label>
 									</div>
 								<?php } ?>
-								<div class="<?php echo $field['type'] == 'help' ? 'epl-full' : 'epl-half-right'; ?>">
-													<?php
-																$val = isset( $field['default'] ) ? $field['default'] : '';
-													if ( isset( $epl_settings[ $field['name'] ] ) ) {
-														$val = $epl_settings[ $field['name'] ];
-													}
-
-													epl_render_html_fields( $field, $val );
-													?>
+								<div class="<?php echo 'help' === $field['type'] ? 'epl-full' : 'epl-half-right'; ?>">
+									<?php
+									$val = isset( $field['default'] ) ? $field['default'] : '';
+									if ( isset( $epl_settings[ $field['name'] ] ) ) {
+										$val = $epl_settings[ $field['name'] ];
+									}
+									epl_render_html_fields( $field, $val );
+									?>
 								</div>
 							</div>
-													<?php
-												}
-												echo '</div>';
-												$counter++;
-					}
-					echo '</div>';
-				}
-				?>
+							<?php } ?>
+						</div>
+						<?php $counter++; ?>
+					<?php } ?>
+					</div>
+				<?php } ?>
 				<div class="epl-clear"></div>
 				<div class="epl-content-footer">
 					<input type="hidden" name="action" value="epl_settings" />
