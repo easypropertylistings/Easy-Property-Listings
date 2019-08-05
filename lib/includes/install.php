78b6acdf808e8c07368dd6c676d8abfe5ffa7694
Merv Barrett <mervb1@gmail.com>
2019-08-05 20:24:24 +0800

    Update install.php

--- lib/includes/install.php
+++ lib/includes/install.php
@@ -7,13 +7,16 @@
  * @copyright   Copyright (c) 2019, Merv Barrett
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
-*/
+ */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Get array of default settings
+ *
  * @return [type] [description]
  * @since 3.3.5
  */
@@ -34,26 +37,26 @@ function epl_get_default_settings() {
 		'display_excerpt_length'               => 10,
 		'display_archive_view_type'            => 'list',
 		'use_fancy_navigation'                 => 0,
-		'label_bond'                           => __('Bond', 'easy-property-listings' ),
-		'label_location'                       => __('Suburb', 'easy-property-listings' ),
-		'label_suburb'                         => __('Suburb', 'easy-property-listings' ),
-		'label_city'                           => __('City', 'easy-property-listings' ),
-		'label_state'                          => __('State', 'easy-property-listings' ),
-		'label_postcode'                       => __('Post Code', 'easy-property-listings' ),
-		'label_home_open'                      => __('Home Open', 'easy-property-listings' ),
-		'label_poa'                            => __('POA', 'easy-property-listings' ),
-		'label_new'                            => __('New', 'easy-property-listings' ),
-		'label_under_offer'                    => __('Under Offer', 'easy-property-listings' ),
-		'label_leased'                         => __('Leased', 'easy-property-listings' ),
-		'label_sold'                           => __('Sold', 'easy-property-listings' ),
-		'widget_label_all'                     => __('All', 'easy-property-listings' ),
-		'widget_label_property'                => __('Buy', 'easy-property-listings' ),
-		'widget_label_land'                    => __('Land', 'easy-property-listings' ),
-		'widget_label_rental'                  => __('Rent', 'easy-property-listings' ),
-		'widget_label_rural'                   => __('Rural', 'easy-property-listings' ),
-		'widget_label_business'                => __('Business', 'easy-property-listings' ),
-		'widget_label_commercial'              => __('Commercial', 'easy-property-listings' ),
-		'widget_label_commercial_land'         => __('Commercial Land', 'easy-property-listings' ),
+		'label_bond'                           => __( 'Bond', 'easy-property-listings' ),
+		'label_location'                       => __( 'Suburb', 'easy-property-listings' ),
+		'label_suburb'                         => __( 'Suburb', 'easy-property-listings' ),
+		'label_city'                           => __( 'City', 'easy-property-listings' ),
+		'label_state'                          => __( 'State', 'easy-property-listings' ),
+		'label_postcode'                       => __( 'Post Code', 'easy-property-listings' ),
+		'label_home_open'                      => __( 'Home Open', 'easy-property-listings' ),
+		'label_poa'                            => __( 'POA', 'easy-property-listings' ),
+		'label_new'                            => __( 'New', 'easy-property-listings' ),
+		'label_under_offer'                    => __( 'Under Offer', 'easy-property-listings' ),
+		'label_leased'                         => __( 'Leased', 'easy-property-listings' ),
+		'label_sold'                           => __( 'Sold', 'easy-property-listings' ),
+		'widget_label_all'                     => __( 'All', 'easy-property-listings' ),
+		'widget_label_property'                => __( 'Buy', 'easy-property-listings' ),
+		'widget_label_land'                    => __( 'Land', 'easy-property-listings' ),
+		'widget_label_rental'                  => __( 'Rent', 'easy-property-listings' ),
+		'widget_label_rural'                   => __( 'Rural', 'easy-property-listings' ),
+		'widget_label_business'                => __( 'Business', 'easy-property-listings' ),
+		'widget_label_commercial'              => __( 'Commercial', 'easy-property-listings' ),
+		'widget_label_commercial_land'         => __( 'Commercial Land', 'easy-property-listings' ),
 		'epl_max_graph_sales_price'            => 2000000,
 		'epl_max_graph_rent_price'             => 2000,
 		'sticker_new_range'                    => 7,
@@ -76,7 +79,7 @@ function epl_get_default_settings() {
 		'epl_video_width'                      => 600,
 		'min_contact_access'                   => 'level_10',
 		'min_reports_access'                   => 'level_10',
-		'activate_post_types'                  => array('property'),
+		'activate_post_types'                  => array( 'property' ),
 		'epl_default_country'                  => 'Australia',
 		'epl_icons_svg_listings'               => 'on',
 		'epl_icons_svg_author'                 => 'on',
@@ -110,13 +113,13 @@ function epl_install() {
 
 	$new_fields_defaults = epl_get_default_settings();
 
-	if(!empty($epl_settings)) {
+	if ( ! empty( $epl_settings ) ) {
 
 		// possible upgrade
-		foreach($new_fields_defaults as $key	=>	$value) {
-			if(!isset($epl_settings[$key])) {
+		foreach ( $new_fields_defaults as $key    => $value ) {
+			if ( ! isset( $epl_settings[ $key ] ) ) {
 				// sure upgrade, fields are not set lets set them for very first time
-				$epl_settings[$key] = $value;
+				$epl_settings[ $key ] = $value;
 			}
 		}
 	} else {
@@ -131,14 +134,14 @@ function epl_install() {
 		update_option( 'epl_version_upgraded_from', $current_version );
 
 	} else {
-		$epl_data = get_plugin_data(EPL_PLUGIN_PATH.'/easy-property-listings.php');
+		$epl_data = get_plugin_data( EPL_PLUGIN_PATH . '/easy-property-listings.php' );
 		update_option( 'epl_version', $epl_data['Version'] );
 	}
 
 	$notice_display = get_transient( 'epl_admin_notices_display' );
 
-	if( !$notice_display ) {
-		set_transient( 'epl_admin_notices_display', true, 60*60*24*14 );
+	if ( ! $notice_display ) {
+		set_transient( 'epl_admin_notices_display', true, 60 * 60 * 24 * 14 );
 	}
 
 	// Bail if activating from network, or bulk
@@ -159,7 +162,7 @@ register_activation_hook( EPL_PLUGIN_FILE, 'epl_install' );
  * @since 2.2
  */
 function epl_uninstall() {
-	update_option('epl_rewrite_rules', false);
+	update_option( 'epl_rewrite_rules', false );
 	flush_rewrite_rules();
 }
 register_deactivation_hook( EPL_PLUGIN_FILE, 'epl_uninstall' );
@@ -205,53 +208,53 @@ function epl_plugin_updates() {
 	$current_version = get_option( 'epl_version' );
 
 	if ( version_compare( $current_version, '1.3', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-1.3.1.php' );
-		update_option( 'epl_version' ,'1.3');
+		include( EPL_PATH_UPDATES . 'epl-1.3.1.php' );
+		update_option( 'epl_version', '1.3' );
 	}
 	if ( version_compare( $current_version, '2.1', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-2.1.php' );
-		update_option( 'epl_version' ,'2.1');
+		include( EPL_PATH_UPDATES . 'epl-2.1.php' );
+		update_option( 'epl_version', '2.1' );
 	}
 	if ( version_compare( $current_version, '2.1.8', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-2.1.8.php' );
-		update_option( 'epl_version' ,'2.1.8');
+		include( EPL_PATH_UPDATES . 'epl-2.1.8.php' );
+		update_option( 'epl_version', '2.1.8' );
 	}
 	if ( version_compare( $current_version, '2.1.11', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-2.1.11.php' );
-		update_option( 'epl_version' ,'2.1.11');
+		include( EPL_PATH_UPDATES . 'epl-2.1.11.php' );
+		update_option( 'epl_version', '2.1.11' );
 	}
 	if ( version_compare( $current_version, '2.2', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-2.2.php' );
-		update_option( 'epl_version' ,'2.2');
+		include( EPL_PATH_UPDATES . 'epl-2.2.php' );
+		update_option( 'epl_version', '2.2' );
 	}
 	if ( version_compare( $current_version, '3.0', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-3.0.php' );
-		update_option( 'epl_version' ,'3.0');
+		include( EPL_PATH_UPDATES . 'epl-3.0.php' );
+		update_option( 'epl_version', '3.0' );
 	}
 	if ( version_compare( $current_version, '30', '==' ) ) { // Fix for 3.0 version number
-		include( EPL_PATH_UPDATES.'epl-3.1.php' );
-		update_option( 'epl_version' ,'3.1');
+		include( EPL_PATH_UPDATES . 'epl-3.1.php' );
+		update_option( 'epl_version', '3.1' );
 	}
 	if ( version_compare( $current_version, '3.1', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-3.1.php' );
-		update_option( 'epl_version' ,'3.1');
+		include( EPL_PATH_UPDATES . 'epl-3.1.php' );
+		update_option( 'epl_version', '3.1' );
 	}
 	if ( version_compare( $current_version, '3.1.16', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-3.1.16.php' );
-		update_option( 'epl_version' ,'3.1.16');
+		include( EPL_PATH_UPDATES . 'epl-3.1.16.php' );
+		update_option( 'epl_version', '3.1.16' );
 	}
 	if ( version_compare( $current_version, '3.2', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-3.2.php' );
-		update_option( 'epl_version' ,'3.2');
+		include( EPL_PATH_UPDATES . 'epl-3.2.php' );
+		update_option( 'epl_version', '3.2' );
 	}
 	if ( version_compare( $current_version, '3.2.2', '<' ) ) {
 		 flush_rewrite_rules();
-		 update_option( 'epl_version' ,'3.2.2');
+		 update_option( 'epl_version', '3.2.2' );
 	}
 	if ( version_compare( $current_version, '3.3', '<' ) ) {
-		include( EPL_PATH_UPDATES.'epl-3.3.php' );
+		include( EPL_PATH_UPDATES . 'epl-3.3.php' );
 		flush_rewrite_rules();
-		update_option( 'epl_version' ,'3.3');
+		update_option( 'epl_version', '3.3' );
 	}
 }
 add_action( 'admin_init', 'epl_plugin_updates' );
