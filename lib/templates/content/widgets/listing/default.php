3c53f0b1f3d43bdae7cab94a0e023c42a776b3b0
prince <developer@realestateconnected.com.au>
2019-08-05 22:58:13 +0530

    Update default.php

--- lib/templates/content/widgets/listing/default.php
+++ lib/templates/content/widgets/listing/default.php
@@ -9,36 +9,30 @@
  * @since       3.0
  */
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * @var array 	 $args
- * @var array 	 $instance
- * @var WP_Query $query
- */
-
-echo $args['before_widget'];
-$title = apply_filters( 'widget_title', $instance['title'] );
-if ( strlen( trim( $title ) ) ) {
-	echo $args['before_title'] . trim( $title ) . $args['after_title'];
+echo esc_attr( $args['before_widget'] );
+$title = apply_filters( 'widget_title', $instance['title'] ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride
+if ( strlen( trim( $title ) ) ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride
+	echo esc_attr( $args['before_title'] ) . esc_attr( trim( $title ) ) . esc_attr( $args['after_title'] );
 }
 
 if ( $query->have_posts() ) {
 	echo '<div class="epl-property-widget-' . esc_attr( $instance['display'] ) . '-wrapper">';
-		if ( 'list' === $instance['display'] ) {
-			echo '<ul>';
-		}
-		while ( $query->have_posts() ) {
-			$query->the_post();
-			epl_property_widget( $instance['display'], $instance['image'], $instance['d_title'], $instance['d_icons'], $instance['more_text'], $instance['d_excerpt'], $instance['d_suburb'], $instance['d_street'], $instance['d_price'], $instance['d_more'],$instance['d_inspection_time'],$instance['d_ical_link'] );
-		}
+	if ( 'list' === $instance['display'] ) {
+		echo '<ul>';
+	}
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		epl_property_widget( $instance['display'], $instance['image'], $instance['d_title'], $instance['d_icons'], $instance['more_text'], $instance['d_excerpt'], $instance['d_suburb'], $instance['d_street'], $instance['d_price'], $instance['d_more'], $instance['d_inspection_time'], $instance['d_ical_link'] );
+	}
 		wp_reset_postdata();
-		if ( 'list' === $instance['display'] ) {
-			echo '</ul>';
-		}
+	if ( 'list' === $instance['display'] ) {
+		echo '</ul>';
+	}
 	echo '</div>';
 }
-echo $args['after_widget'];
+echo esc_attr( $args['after_widget'] );
