6095525a9d3c6b57ec5f4a708b95e096007a9734
prince <developer@realestateconnected.com.au>
2019-08-07 22:45:00 +0530

    Update archive-listing.php

--- lib/templates/themes/twentyfourteen/archive-listing.php
+++ lib/templates/themes/twentyfourteen/archive-listing.php
@@ -9,8 +9,8 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area epl-archive-default <?php echo epl_get_active_theme_name(); ?>">
-		<div id="content" class="site-content <?php echo epl_template_class( 'twentyfourteen', 'archive' ); ?>" role="main">
+	<section id="primary" class="content-area epl-archive-default <?php echo esc_attr( epl_get_active_theme_name() ); ?>">
+		<div id="content" class="site-content <?php echo esc_attr( epl_template_class( 'twentyfourteen', 'archive' ) ); ?>" role="main">
 
 			<?php if ( have_posts() ) : ?>
 
@@ -19,32 +19,34 @@ get_header(); ?>
 					<?php do_action( 'epl_the_archive_title' ); ?>
 				</h1>
 			</header><!-- .page-header -->
-			<?php
+				<?php
 					do_action( 'epl_property_loop_start' );
 					// Start the Loop.
-					while ( have_posts() ) : the_post();
+				while ( have_posts() ) :
+					the_post();
 
-						/*
-						 * Include the post format-specific template for the content. If you want to
-						 * use this in a child theme, then include a file called called content-___.php
-						 * (where ___ is the post format) and that will be used instead.
-						 */
-						do_action('epl_property_blog');
+					/*
+					 * Include the post format-specific template for the content. If you want to
+					 * use this in a child theme, then include a file called called content-___.php
+					 * (where ___ is the post format) and that will be used instead.
+					 */
+					do_action( 'epl_property_blog' );
 
 					endwhile;
 
 					do_action( 'epl_property_loop_end' );
 
 					// Previous/next page navigation.
-					do_action('epl_pagination');
+					do_action( 'epl_pagination' );
 
 				else :
-					?><div class="hentry">
+					?>
+					<div class="hentry">
 						<?php do_action( 'epl_property_search_not_found' ); ?>
 					</div>
 					<?php
 				endif;
-			?>
+				?>
 		</div><!-- #content -->
 	</section><!-- #primary -->
 
