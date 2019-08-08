fc146ac3b7367e85f30ce01d349a3382f0615b1b
prince <developer@realestateconnected.com.au>
2019-08-09 00:57:49 +0530

    Update epl-3.1.16.php

--- lib/updates/epl-3.1.16.php
+++ lib/updates/epl-3.1.16.php
@@ -1,6 +1,20 @@
 <?php
+
+/**
+ * Settings updates for 3.1.16
+ *
+ * @package EPL
+ * @subpackage updates
+ * @since 3.1.16
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 $notice_display = get_transient( 'epl_admin_notices_display' );
 
-if( !$notice_display ) {
-	set_transient( 'epl_admin_notices_display', true, 60*60*24*14 );
+if ( ! $notice_display ) {
+	set_transient( 'epl_admin_notices_display', true, 60 * 60 * 24 * 14 );
 }
