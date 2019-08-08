9b6e145eda634cd6f38e41ed582b26c083f85317
prince <developer@realestateconnected.com.au>
2019-08-09 00:57:45 +0530

    Update epl-2.2.php

--- lib/updates/epl-2.2.php
+++ lib/updates/epl-2.2.php
@@ -1,26 +1,40 @@
 <?php
+
+/**
+ * Settings updates for 2.2
+ *
+ * @package EPL
+ * @subpackage updates
+ * @since 2.2
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 $epl_settings = epl_settings();
 
 $new_fields_defaults = array(
-	'label_city'				=> __('City','easy-property-listings'),
-	'label_state'				=> __('State', 'easy-property-listings'),
-	'epl_feeling_lucky'			=> 'off',
-	'epl_lucky_disable_single_thumb'	=> 'off',
-	'epl_lucky_disable_theme_single_thumb'	=> 'off',
-	'epl_lucky_disable_archive_thumb'	=> 'off',
-	'epl_lucky_disable_epl_archive_thumb'	=> 'off',
-	'display_archive_view_type'		=> 'list',
-	'use_fancy_navigation'			=> 0,
-	'epl_enable_city_field'			=> 'no',
-	'epl_enable_country_field'		=> 'no',
-	'epl_use_core_css'			=> 'off',
-	'uninstall_on_delete'			=> 'no',
+	'label_city'                           => __( 'City', 'easy-property-listings' ),
+	'label_state'                          => __( 'State', 'easy-property-listings' ),
+	'epl_feeling_lucky'                    => 'off',
+	'epl_lucky_disable_single_thumb'       => 'off',
+	'epl_lucky_disable_theme_single_thumb' => 'off',
+	'epl_lucky_disable_archive_thumb'      => 'off',
+	'epl_lucky_disable_epl_archive_thumb'  => 'off',
+	'display_archive_view_type'            => 'list',
+	'use_fancy_navigation'                 => 0,
+	'epl_enable_city_field'                => 'no',
+	'epl_enable_country_field'             => 'no',
+	'epl_use_core_css'                     => 'off',
+	'uninstall_on_delete'                  => 'no',
 );
 
-foreach($new_fields_defaults as $key	=>	$value) {
-	if(!isset($epl_settings[$key])) {
-		// sure upgrade, fields are not set lets set them for very first time
-		$epl_settings[$key] = $value;
+foreach ( $new_fields_defaults as $key    => $value ) {
+	if ( ! isset( $epl_settings[ $key ] ) ) {
+		// sure upgrade, fields are not set lets set them for very first time.
+		$epl_settings[ $key ] = $value;
 	}
 }
 update_option( 'epl_settings', $epl_settings );
