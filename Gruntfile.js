2497c63f56381b6333d29abe137fe661d28683f3
prince <developer@realestateconnected.com.au>
2019-08-09 10:10:09 +0530

    updated grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,6 +59,40 @@ module.exports = function( grunt ) {
 			}
 		},
 
+		checktextdomain: {
+			options:{
+				text_domain: 'easy-property-listings',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,3,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d',
+					' __ngettext:1,2,3d',
+					'__ngettext_noop:1,2,3d',
+					'_c:1,2d',
+					'_nc:1,2,4c,5d'
+				]
+			},
+			files: {
+				src: [
+					'**/*.php', // Include all files
+					'!node_modules/**', // Exclude node_modules/
+					'!apigen/**'// Exclude apigen/
+				],
+				expand: true
+			}
+		},
+
 		// Generate POT files.
 		makepot: {
 			options: {
@@ -105,6 +139,7 @@ module.exports = function( grunt ) {
 	grunt.registerTask( 'default', [
 		'uglify',
 		'css',
+		'checktextdomain',
 		'makepot'
 	]);
 
