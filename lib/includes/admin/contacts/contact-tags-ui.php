b6840d08c4847b904c84810fdc69dedead36c981
prince <developer@realestateconnected.com.au>
2019-08-09 22:44:38 +0530

    Update contact-tags-ui.php

--- lib/includes/admin/contacts/contact-tags-ui.php
+++ lib/includes/admin/contacts/contact-tags-ui.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class='wrap'>
 	<div class="epl-contact-all-tags-wrap">
 		<div class="epl-contact-all-tags-header">
-			<h2 class="epl-contact-all-tags-title"><?php _e( 'Tags', 'easy-property-listings' ); ?></h2>
+			<h2 class="epl-contact-all-tags-title"><?php esc_html_e( 'Tags', 'easy-property-listings' ); ?></h2>
 			<div class="epl-contact-all-tags-menu">
 				<?php do_action( 'epl_contact_pre_all_tags_menu' ); ?>
 
@@ -29,9 +29,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 				$contact_tags = get_terms( 'epl_contact_tag', array( 'hide_empty' => false ) );
 				if ( ! empty( $contact_tags ) ) {
 					if ( ! is_wp_error( $contact_tags ) ) {
-						foreach ( $contact_tags as $term ) {
+						foreach ( $contact_tags as $term ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride
 							$bgcolor = epl_get_contact_tag_bgcolor( $term->term_id );
-							echo '<span><button class="epl-tag-btn" data-accent="' . $bgcolor . '" data-id="' . $term->term_id . '" id="contact-tag-' . $term->term_id . '" style="color:' . $bgcolor . '">' . esc_html( $term->name ) . '<span class="epl-term-count">' . $term->count . '</button></span>';
+							echo '<span><button class="epl-tag-btn" data-accent="' . esc_attr( $bgcolor ) . '" data-id="' . esc_attr( $term->term_id ) . '" id="contact-tag-' . esc_attr( $term->term_id ) . '" style="color:' . esc_attr( $bgcolor ) . '">' . esc_html( $term->name ) . '<span class="epl-term-count">' . esc_attr( $term->count ) . '</button></span>';
 						}
 					}
 				}
