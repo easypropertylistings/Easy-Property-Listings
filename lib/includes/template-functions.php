844d32c55917c4d7ebc9ff1b3b09648fb6681f88
mervb1@gmail.com <mervb1@gmail.com>
2019-09-16 16:49:39 +0800

    * New: Added option allowing passing of $full parameter as false to restrict output to street items only.

--- lib/includes/template-functions.php
+++ lib/includes/template-functions.php
@@ -622,13 +622,14 @@ function epl_property_get_the_full_address() {
  * @since 1.0
  * @since 3.3.3 Revised.
  * @since 3.4.8 Corrected separator location to appear AFTER the street name and options to control display.
+ * @since 3.4.9 Added option allowing passing of $full parameter as false to restrict output to street items only.
  */
-function epl_property_the_address( $street_separator = true, $separator_symbol = ',' ) {
+function epl_property_the_address( $full = true, $street_separator = true, $separator_symbol = ',' ) {
 
 	global $property, $epl_settings;
 
-	if ( ! is_bool( $street_separator ) ) {
-		$street_separator = true;
+	if ( ! is_bool( $full ) ) {
+		$full = true;
 	}
 
 	$epl_property_address_separator        = apply_filters( 'epl_property_address_separator', ',' );
@@ -640,10 +641,21 @@ function epl_property_the_address( $street_separator = true, $separator_symbol =
 		<span class="item-street"><?php echo wp_kses_post( $property->get_formatted_property_address( $street_separator, $separator_symbol ) ); ?></span>
 	<?php } ?>
 
-	<span class="entry-title-sub">
-		<?php
-		if ( 'commercial' === $property->post_type || 'business' === $property->post_type ) {
-			if ( 'yes' === $property->get_property_meta( 'property_com_display_suburb' ) || 'yes' === $property->get_property_meta( 'property_address_display' ) ) {
+	<?php
+	if ( true === $full ) {
+	?>
+		<span class="entry-title-sub">
+			<?php
+			if ( 'commercial' === $property->post_type || 'business' === $property->post_type ) {
+				if ( 'yes' === $property->get_property_meta( 'property_com_display_suburb' ) || 'yes' === $property->get_property_meta( 'property_address_display' ) ) {
+					?>
+					<span class="item-suburb"><?php echo esc_attr( $property->get_property_meta( 'property_address_suburb' ) ); ?></span>
+					<?php
+					if ( true === $epl_property_address_separator_suburb && strlen( trim( $property->get_property_meta( 'property_address_suburb' ) ) ) ) {
+						echo '<span class="item-separator">' . esc_attr( $epl_property_address_separator ) . '</span>';
+					}
+				}
+			} else {
 				?>
 				<span class="item-suburb"><?php echo esc_attr( $property->get_property_meta( 'property_address_suburb' ) ); ?></span>
 				<?php
@@ -651,41 +663,34 @@ function epl_property_the_address( $street_separator = true, $separator_symbol =
 					echo '<span class="item-separator">' . esc_attr( $epl_property_address_separator ) . '</span>';
 				}
 			}
-		} else {
 			?>
-			<span class="item-suburb"><?php echo esc_attr( $property->get_property_meta( 'property_address_suburb' ) ); ?></span>
-			<?php
-			if ( true === $epl_property_address_separator_suburb && strlen( trim( $property->get_property_meta( 'property_address_suburb' ) ) ) ) {
-				echo '<span class="item-separator">' . esc_attr( $epl_property_address_separator ) . '</span>';
-			}
-		}
-		?>
 
-		<?php
-		if ( 'yes' === $property->get_epl_settings( 'epl_enable_city_field' ) ) {
-			?>
-			<span class="item-city"><?php echo esc_attr( $property->get_property_meta( 'property_address_city' ) ); ?></span>
 			<?php
-			if ( true === $epl_property_address_separator_city && strlen( trim( $property->get_property_meta( 'property_address_city' ) ) ) ) {
-				echo '<span class="item-separator">' . esc_attr( $epl_property_address_separator ) . '</span>';
+			if ( 'yes' === $property->get_epl_settings( 'epl_enable_city_field' ) ) {
+				?>
+				<span class="item-city"><?php echo esc_attr( $property->get_property_meta( 'property_address_city' ) ); ?></span>
+				<?php
+				if ( true === $epl_property_address_separator_city && strlen( trim( $property->get_property_meta( 'property_address_city' ) ) ) ) {
+					echo '<span class="item-separator">' . esc_attr( $epl_property_address_separator ) . '</span>';
+				}
 			}
-		}
-		?>
-		<span class="item-state"><?php echo esc_attr( $property->get_property_meta( 'property_address_state' ) ); ?></span>
-		<span class="item-pcode"><?php echo esc_attr( $property->get_property_meta( 'property_address_postal_code' ) ); ?></span>
-		<?php
-		if ( 'yes' === $property->get_epl_settings( 'epl_enable_country_field' ) ) {
 			?>
-			<span class="item-country"><?php echo esc_attr( $property->get_property_meta( 'property_address_country' ) ); ?></span>
+			<span class="item-state"><?php echo esc_attr( $property->get_property_meta( 'property_address_state' ) ); ?></span>
+			<span class="item-pcode"><?php echo esc_attr( $property->get_property_meta( 'property_address_postal_code' ) ); ?></span>
 			<?php
-		}
-		?>
-	</span>
+			if ( 'yes' === $property->get_epl_settings( 'epl_enable_country_field' ) ) {
+				?>
+				<span class="item-country"><?php echo esc_attr( $property->get_property_meta( 'property_address_country' ) ); ?></span>
+				<?php
+			}
+			?>
+		</span>
 	<?php
+	}
 }
-add_action( 'epl_property_title', 'epl_property_the_address', 10, 2 );
-add_action( 'epl_property_tab_address', 'epl_property_the_address', 10, 2 );
-add_action( 'epl_property_address', 'epl_property_the_address', 10, 2 );
+add_action( 'epl_property_title', 'epl_property_the_address', 10, 3 );
+add_action( 'epl_property_tab_address', 'epl_property_the_address', 10, 3 );
+add_action( 'epl_property_address', 'epl_property_the_address', 10, 3 );
 
 /**
  * Suburb Name Kept for listing templates extensions which use this function
