e149677eeb161239dc5bdaca20aaf15611dd0469
mervb1@gmail.com <mervb1@gmail.com>
2020-03-20 17:19:05 +0800

    Update hook-read-more.php

--- lib/hooks/hook-read-more.php
+++ lib/hooks/hook-read-more.php
@@ -23,9 +23,9 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.0.0
  */
 function epl_button_read_more( $label ) {
-    if ( empty ( $label ) ) {
-	    $label = __( 'Read More ', 'easy-property-listings' );
-    }
+	if ( empty( $label ) ) {
+		$label = __( 'Read More ', 'easy-property-listings' );
+	}
 	$label = apply_filters( 'epl_button_label_read_more', $label );
 	?><button type="button" class="epl-button epl-read-more" onclick="location.href='<?php the_permalink(); ?>'"><?php echo esc_html( $label ); ?></button>
 	<?php
