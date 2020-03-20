10a13cc8f2dc494815a58a6f4432cf55000e91ad
mervb1@gmail.com <mervb1@gmail.com>
2020-03-20 17:15:57 +0800

    New: Read More button action hook epl_button_read_more to use in templates accepts a passed $label.

--- lib/hooks/hook-read-more.php
+++ lib/hooks/hook-read-more.php
@@ -17,9 +17,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Outputs a Read More button on the loop listing templates
  *
+ * @param string $label Pass a custom label from the template hook.
+ *
+ * @since 3.4.23 Added epl_button_read_more hook to output button on templates.
  * @since 1.0.0
  */
-function epl_button_read_more() {
-	?><button type="button" class="epl-button epl-read-more" onclick="location.href='<?php the_permalink(); ?>'"><?php echo apply_filters( 'epl_button_label_read_more', esc_html__( 'Read More ', 'easy-property-listings' ) ); ?></button>
+function epl_button_read_more( $label ) {
+    if ( empty ( $label ) ) {
+	    $label = __( 'Read More ', 'easy-property-listings' );
+    }
+	$label = apply_filters( 'epl_button_label_read_more', $label );
+	?><button type="button" class="epl-button epl-read-more" onclick="location.href='<?php the_permalink(); ?>'"><?php echo esc_html( $label ); ?></button>
 	<?php
 }
+add_action( 'epl_button_read_more', 'epl_button_read_more', 10, 1 );
