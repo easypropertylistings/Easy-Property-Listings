f16d33669af81451fbbe8c38040a997f0bec2771
Merv Barrett <merv@realestateconnected.com.au>
2021-09-02 12:35:54 +0800

    Update shortcode-epl-tabs.php

--- lib/shortcodes/shortcode-epl-tabs.php
+++ lib/shortcodes/shortcode-epl-tabs.php
@@ -4,9 +4,9 @@
  *
  * @package     EPL
  * @subpackage  Shortcode/epl_tabs
- * @copyright   Copyright (c) 2020, Merv Barrett
+ * @copyright   Copyright (c) 2021, Merv Barrett
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       1.0
+ * @since       3.5.0
  */
 
 // Exit if accessed directly.
@@ -20,13 +20,13 @@ if ( ! defined( 'ABSPATH' ) ) {
  * This shortcode creates tabs with title and content
  * [epl_tabs]
  *
- * @param array $atts Shortcode attributes.
+ * @param array $atts Attributes.
+ * @param mixed $atts Content.
  *
  * @return false|string
- * @since       1.0
+ * @since       3.5.0
  */
-
-function epl_shortcode_epl_tabs_callback( $atts=array(), $content ) {
+function epl_shortcode_epl_tabs_callback( $atts = array(), $content ) {
 	global $epl_shortcode_tabs;
 	$epl_shortcode_tabs = new EPL_Tabs_Shortcode( $atts, $content );
 
@@ -35,7 +35,18 @@ function epl_shortcode_epl_tabs_callback( $atts=array(), $content ) {
 }
 add_shortcode( 'epl_tabs', 'epl_shortcode_epl_tabs_callback' );
 
-function epl_shortcode_epl_tab_callback(  $atts=array(), $content ) {
+/**
+ * EPL Tabs Shortcode [epl_tab]
+ *
+ * This shortcode creates a single tab
+ *
+ * @param array $atts Attributes.
+ * @param mixed $atts Content.
+ *
+ * @return false|string
+ * @since       3.5.0
+ */
+function epl_shortcode_epl_tab_callback(  $atts = array(), $content ) {
 	global $epl_shortcode_tabs;
 
 	if( !is_a( $epl_shortcode_tabs, 'EPL_Tabs_Shortcode') ) {
