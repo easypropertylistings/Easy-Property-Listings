3df7ca333fc2faf80979e485a24b583631e62ae8
Merv Barrett <mervb1@gmail.com>
2022-12-20 15:08:23 +0800

    Avada single template updated

--- lib/templates/themes/avada/single-listing.php
+++ lib/templates/themes/avada/single-listing.php
@@ -16,25 +16,20 @@ global $post;
 ?>
 <?php get_header(); ?>
 
-<section id="content" <?php Avada()->layout->add_style( 'content_style' ); ?>>
-	<?php $post_pagination = get_post_meta( $post->ID, 'pyre_post_pagination', true ); ?>
-	<?php if ( ( Avada()->settings->get( 'blog_pn_nav' ) && 'no' !== $post_pagination ) || ( ! Avada()->settings->get( 'blog_pn_nav' ) && 'yes' === $post_pagination ) ) : ?>
+<section id="content" style="<?php esc_attr_e( apply_filters( 'awb_content_tag_style', '' ) ); ?>">
+	<?php if ( fusion_get_option( 'blog_pn_nav' ) ) : ?>
 		<div class="single-navigation clearfix">
-			<?php previous_post_link( '%link', esc_attr__( 'Previous', 'easy-property-listings' ) ); ?>
-			<?php next_post_link( '%link', esc_attr__( 'Next', 'easy-property-listings' ) ); ?>
+			<?php previous_post_link( '%link', esc_attr__( 'Previous', 'Avada' ) ); ?>
+			<?php next_post_link( '%link', esc_attr__( 'Next', 'Avada' ) ); ?>
 		</div>
 	<?php endif; ?>
 
 	<?php while ( have_posts() ) : ?>
 		<?php the_post(); ?>
-		<?php
-			do_action( 'epl_property_single' );
-			comments_template(); // Include comments template.
-		?>
+		
+		<?php do_action( 'epl_property_single' ); ?>
+		
 	<?php endwhile; ?>
-	<?php wp_reset_postdata(); ?>
 </section>
 <?php do_action( 'avada_after_content' ); ?>
-<?php
-get_footer();
-
+<?php get_footer(); ?>
