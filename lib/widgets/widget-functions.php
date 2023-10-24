1ecdf9b0d58c337f85eded4afefe78f1a1012b26
Merv Barrett <mervb1@gmail.com>
2023-10-24 17:10:35 +0800

    INstance id moved

--- lib/widgets/widget-functions.php
+++ lib/widgets/widget-functions.php
@@ -60,14 +60,6 @@ function epl_search_widget_fields() {
 				'order'   => 30,
 			),
 			array(
-				'key'     => 'instance_id',
-				'label'   => __( 'Form Instance ID', 'easy-property-listings' ),
-				'type'    => 'number',
-				'default' => '',
-				'order'   => 300,
-				'help'    => __( 'Leave empty to generate it automatically', 'easy-property-listings' ),
-			),
-			array(
 				'key'     => 'show_property_status_frontend',
 				'label'   => __( 'Status', 'easy-property-listings' ),
 				'default' => 'off',
@@ -221,6 +213,14 @@ function epl_search_widget_fields() {
 				'default' => 'on',
 				'type'    => 'checkbox',
 				'order'   => 240,
+			),
+			array(
+				'key'     => 'instance_id',
+				'label'   => __( 'Form Instance ID', 'easy-property-listings' ),
+				'type'    => 'number',
+				'default' => '',
+				'order'   => 290,
+				'help'    => __( 'Leave empty to generate it automatically', 'easy-property-listings' ),
 			),
 			array(
 				'key'     => 'submit_label',
