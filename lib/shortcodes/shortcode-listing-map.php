d2c743bd5024afd20094e2a70719802449771a89
Merv Barrett <mervb1@gmail.com>
2023-11-02 12:47:25 +0800

    beautify

--- lib/shortcodes/shortcode-listing-map.php
+++ lib/shortcodes/shortcode-listing-map.php
@@ -28,11 +28,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 function epl_shortcode_googlemap_callback( $atts, $content = null ) {
 	global $property;
 
-        $id = 0;
+	$id = 0;
 
-        if( !is_null( $property ) ) {
-                $id = $property->post->ID;
-        }
+	if ( ! is_null( $property ) ) {
+		$id = $property->post->ID;
+	}
 
 	$atts = shortcode_atts(
 		array(
@@ -46,18 +46,17 @@ function epl_shortcode_googlemap_callback( $atts, $content = null ) {
 		$atts
 	);
 
-        if( empty( $atts['cord'] ) && empty( $atts['q']  ) ) {
-                return '';
-        }
+	if ( empty( $atts['cord'] ) && empty( $atts['q'] ) ) {
+		return '';
+	}
 
-        if( !is_epl_post() ) {
-
-                epl_shortcode_googlemap_enqueue();
-        }
+	if ( ! is_epl_post() ) {
+		epl_shortcode_googlemap_enqueue();
+	}
 
 	return '<div class="epl-tab-section epl-section-map">
-				<div style="width:' . esc_attr( $atts['width'] ) . '; height:' . esc_attr( $atts['height'] ) . 'px" data-suburb_mode="' . esc_attr( $atts['suburb_mode'] ) . '" data-cord="' . esc_attr( $atts['cord'] ) . '" data-zoom="' . esc_attr( $atts['zoom'] ) . '" data-id="' . esc_attr( $id ) . '" data-address="' . esc_attr( $atts['q'] ) . '" id="epl-default-map">
-			</div> </div>';
+			<div style="width:' . esc_attr( $atts['width'] ) . '; height:' . esc_attr( $atts['height'] ) . 'px" data-suburb_mode="' . esc_attr( $atts['suburb_mode'] ) . '" data-cord="' . esc_attr( $atts['cord'] ) . '" data-zoom="' . esc_attr( $atts['zoom'] ) . '" data-id="' . esc_attr( $id ) . '" data-address="' . esc_attr( $atts['q'] ) . '" id="epl-default-map"></div>
+		</div>';
 }
 add_shortcode( 'listing_map', 'epl_shortcode_googlemap_callback' );
 
@@ -69,13 +68,13 @@ add_shortcode( 'listing_map', 'epl_shortcode_googlemap_callback' );
  */
 function epl_shortcode_googlemap_enqueue() {
 
-        $googleapiurl       = 'https://maps.googleapis.com/maps/api/js?v=3.exp&callback=Function.prototype';
-        $epl_google_api_key = epl_get_option( 'epl_google_api_key' );
-        if ( ! empty( $epl_google_api_key ) ) {
-                $googleapiurl = $googleapiurl . '&key=' . epl_get_option( 'epl_google_api_key' );
-        }
+		$googleapiurl       = 'https://maps.googleapis.com/maps/api/js?v=3.exp&callback=Function.prototype';
+		$epl_google_api_key = epl_get_option( 'epl_google_api_key' );
+	if ( ! empty( $epl_google_api_key ) ) {
+			$googleapiurl = $googleapiurl . '&key=' . epl_get_option( 'epl_google_api_key' );
+	}
 
-        if ( epl_get_option( 'epl_disable_google_api' ) !== 'on' ) {
-                wp_enqueue_script( 'google-map-v-3', $googleapiurl, array(), EPL_PROPERTY_VER, false );
-        }
+	if ( epl_get_option( 'epl_disable_google_api' ) !== 'on' ) {
+			wp_enqueue_script( 'google-map-v-3', $googleapiurl, array(), EPL_PROPERTY_VER, false );
+	}
 }
