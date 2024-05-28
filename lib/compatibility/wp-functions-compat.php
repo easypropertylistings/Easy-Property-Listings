3b6bf28fe31cf2ff2e086e0faac215219d2ee482
Prince Singh <prince.kumar.cse@gmail.com>
2024-05-28 14:20:14 +0530

    added wp compat functions for epl

--- lib/compatibility/wp-functions-compat.php
+++ lib/compatibility/wp-functions-compat.php
@@ -30,3 +30,42 @@ if ( ! function_exists( 'shortcode_exists' ) ) {
 		return array_key_exists( $tag, $shortcode_tags );
 	}
 }
+
+if( !function_exists( 'wp_timezone_string' ) ) {
+
+        function wp_timezone_string() {
+                $timezone_string = get_option( 'timezone_string' );
+        
+                if ( $timezone_string ) {
+                        return $timezone_string;
+                }
+        
+                $offset  = (float) get_option( 'gmt_offset' );
+                $hours   = (int) $offset;
+                $minutes = ( $offset - $hours );
+        
+                $sign      = ( $offset < 0 ) ? '-' : '+';
+                $abs_hour  = abs( $hours );
+                $abs_mins  = abs( $minutes * 60 );
+                $tz_offset = sprintf( '%s%02d:%02d', $sign, $abs_hour, $abs_mins );
+        
+                return $tz_offset;
+        }
+        
+}
+
+if( !function_exists( 'wp_timezone' ) ) {
+
+        function wp_timezone() {
+                return new DateTimeZone( wp_timezone_string() );
+        }
+        
+}
+
+if( !function_exists( 'current_datetime' ) ) {
+
+        function current_datetime() {
+                return new DateTimeImmutable( 'now', wp_timezone() );
+        }
+        
+}
